7a1a52e902e3248fa0a165877be44fd2be4a2ae6
Pablo de la Pena <p@shedcollective.org>
2013-03-06 17:00:10 +0000

    Updated the log folder is_writeable catcher to include the URI - try and help when debugging.

--- libraries/CORE_NAILS_Log.php
+++ libraries/CORE_NAILS_Log.php
@@ -18,15 +18,40 @@ class CORE_NAILS_Log extends CI_Log
 			if ( ! is_writeable( $this->_log_path ) ) :
 			
 				//	Kick up a fuss and tell Shed
+				
+				if ( isset( $_SERVER['REQUEST_URI'] ) ) :
+				
+					$_uri = $_SERVER['REQUEST_URI'];
+				
+				else :
+				
+					//	Most likely on the CLI
+					if ( php_sapi_name() == 'cli' ) :
+					
+						$_uri = 'CLI: index.php ' . implode( ' ', $_SERVER['argv'] );
+					
+					else :
+					
+						$_uri = 'Unable to determine URI';
+					
+					endif;
+				
+				endif;
+				
 				$message	= strtoupper($level).' '.((strtoupper($level) == 'INFO') ? ' -' : '-').' '.date($this->_date_fmt). ' --> '.$msg."\n";
 				
-				$_subject	= 'Log folders are not writeable on ' . APP_NAME;
+				$_subject	= '!! Log folders are not writeable on ' . APP_NAME;
 				$_message	= 'I just tried to write to the log folder for ' . APP_NAME . ' and found them not to be writeable.' . "\n";
 				$_message	.= '' . "\n";
 				$_message	.= 'Get this fixed ASAP - I\'ll bug you every time this happens.' . "\n";
 				$_message	.= '' . "\n";
-				$_message	.= 'FYI, the log folder was ' . $this->_log_path . ' and the entry was:' . "\n";
-				$_message	.= $message;
+				$_message	.= 'FYI, the entry was:' . "\n";
+				$_message	.= '' . "\n";
+				$_message	.= $message . "\n";
+				$_message	.= '' . "\n";
+				$_message	.= 'The calling URI was:' . "\n";
+				$_message	.= '' . "\n";
+				$_message	.= $_uri . "\n";
 				
 				send_developer_mail( $_subject , $_message );
 			
