08447d5ff3ae6f5e26f3a6f90fd90311fae44200
Pablo de la Pena <p@shedcollective.org>
2013-04-29 10:30:22 +0100

    Added placeholder files for new events and testimonials modules.

---
+++ modules/admin/language/english/admin_testimonials_lang.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Name:			Testimonials Langfile
+*
+* Description:  Generic language file for Nails. Admin Testimonials
+* 
+*/
+
+	//	Generic for module
+	$lang['testimonials_module_name']		= 'Testimonials';
+	
+	// --------------------------------------------------------------------------
+	
+	//	Nav
+	$lang['testimonials_nav_index']		= 'Manage Testimonials';
+	
+	// --------------------------------------------------------------------------
+	
+	//	Manage
+	$lang['testimonials_manage_title']	= 'Manage Testiomonials';
+	
+	
+	
