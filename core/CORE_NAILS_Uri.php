3fb43162fd8d25d2949d2ba78ff4b816f2f0d75b
Pablo de la Pena <p@shedcollective.org>
2013-07-05 16:19:59 +0100

    Inline docs for CORE_NAILS_URI Class

--- core/CORE_NAILS_Uri.php
+++ core/CORE_NAILS_Uri.php
@@ -2,6 +2,14 @@
 
 class CORE_NAILS_URI extends CI_URI
 {
+	/**
+	 * Overridding _set_uri_string so that the URI is always interpreted as lowercase.
+	 * Prevents a bug where fatal errors occur when the URI is in capitals.
+	 *
+	 * @access	public
+	 * @param	string	$str The URI string
+	 * @return	void
+	 */
 	function _set_uri_string($str)
 	{
 		return parent::_set_uri_string( strtolower( $str ) );
