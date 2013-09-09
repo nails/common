d5a76ebb9c9ee92e2bdc11977898a6144422d27d
Pablo de la Pena <p@shedcollective.org>
2013-09-09 17:58:47 +0100

    Replaced deprecated use of eval (in a regex) with preg_match_callback()

--- helpers/tools_helper.php
+++ helpers/tools_helper.php
@@ -35,22 +35,22 @@ if ( ! function_exists( 'map' ) )
 if ( ! function_exists( 'special_chars' ) )
 {
 	function special_chars( $string )
-	{	
+	{
 		/* Only do the slow convert if there are 8-bit characters */
 		/* avoid using 0xA0 (\240) in ereg ranges. RH73 does not like that */
 		if ( ! preg_match( "/[\200-\237]/", $string ) and ! preg_match( "/[\241-\377]/", $string ) )
-			return $string; 
-		
+			return $string;
+
 		// decode three byte unicode characters
 		$string = preg_replace( "/([\340-\357])([\200-\277])([\200-\277])/e",
 		"'&#'.((ord('\\1')-224)*4096 + (ord('\\2')-128)*64 + (ord('\\3')-128)).';'",
 		$string );
-		
-		// decode two byte unicode characters 
+
+		// decode two byte unicode characters
 		$string = preg_replace( "/([\300-\337])([\200-\277])/e",
 		"'&#'.((ord('\\1')-192)*64+(ord('\\2')-128)).';'",
 		$string );
-		
+
 		return $string;
 	}
 }
@@ -69,19 +69,19 @@ if ( ! function_exists( 'special_chars' ) )
 if ( ! function_exists( 'format_bytes' ) )
 {
 	function format_bytes( $bytes, $precision = 2 )
-	{ 
-		$units = array( 'B', 'KB', 'MB', 'GB', 'TB' ); 
+	{
+		$units = array( 'B', 'KB', 'MB', 'GB', 'TB' );
 
-		$bytes = max( $bytes, 0); 
-		$pow = floor( ( $bytes ? log( $bytes ) : 0 ) / log( 1024 ) ); 
-		$pow = min( $pow, count( $units ) - 1 ); 
-		
+		$bytes = max( $bytes, 0);
+		$pow = floor( ( $bytes ? log( $bytes ) : 0 ) / log( 1024 ) );
+		$pow = min( $pow, count( $units ) - 1 );
+
 		//	Uncomment one of the following alternatives
 		//$bytes /= pow(1024, $pow);
-		$bytes /= ( 1 << ( 10 * $pow ) ); 
-		
+		$bytes /= ( 1 << ( 10 * $pow ) );
+
 		$var = round( $bytes, $precision ) . ' ' . $units[$pow];
-		return preg_replace( '/(.+?)\.(.*?)/e', 'number_format(\\1).\'.\\2\'', $var );
+		return preg_replace_callback( '/(.+?)\.(.*?)/', function( $matches ) { return number_format($matches[1]) . '.' . $matches[2]; }, $var );
 	}
 }
 
@@ -99,7 +99,7 @@ if ( ! function_exists( 'format_bytes' ) )
 function generate_reference( $length = 10 )
 {
 	$characters = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
-	$string = '';    
+	$string = '';
 	for ( $p = 0; $p < $length; $p++ ) {
 		$string .= $characters[ mt_rand( 0, strlen( $characters ) - 1) ];
 	}
@@ -119,13 +119,13 @@ function generate_reference( $length = 10 )
  */
 function int_to_word( $number )
 {
-	
+
 	$words = array(
-	
+
 		'zero', 'one', 'two', 'three', 'four',
 		'five', 'six', 'seven', 'eight', 'nine'
 	);
-	
+
 	return ( (int) $number >= count( $words ) ) ?  $number : $words[ $number ];
 }
 
@@ -143,13 +143,13 @@ function int_to_word( $number )
 function string_to_boolean( $string )
 {
 	if ( $string && strtoupper( $string ) !== "FALSE") :
-	
+
 		return TRUE;
-	
+
 	else:
-	
+
 		return FALSE;
-	
+
 	endif;
 }
 
