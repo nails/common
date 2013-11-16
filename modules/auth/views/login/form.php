fee42248744bc40ad7cc4ef524fbed72ee19960f
Pablo de la Pena <p@shedcollective.org>
2013-11-16 14:18:15 +0000

    Fixed bug which was preventing people form logging in

--- modules/auth/views/login/form.php
+++ modules/auth/views/login/form.php
@@ -2,52 +2,54 @@
 
 	/**
 	 *	THE LOGIN FORM
-	 *	
+	 *
 	 *	This view contains only the basic form required for logging a user in. The controller
 	 *	will look for an app version of the file  first and load that up. It will fall back
 	 *	to the empty Nails view if not available (which includes some basic styling so
 	 *	as not to look totally rubbish).
-	 *	
+	 *
 	 *	You can completely overload this view by creating a view at:
-	 *	
+	 *
 	 *	application/views/auth/login/form
-	 *	
+	 *
 	 **/
-	
+
 	// --------------------------------------------------------------------------
-	
+
 	/**
 	 *	LOGIN ERRORS
-	 *	
+	 *
 	 *	Only individual field errors are shown, generic erros (such as too many attempted logins)
 	 *	should be handled by the containing header files.
-	 *	
+	 *
 	 **/
-	 
-	
+
+
 	//	Form attributes
 	$attr = array(
-	
+
 		'id'	=> 'login-form',
 		'class'	=> 'container nails-default-form'
-		
+
 	);
-	
+
 	//	If there's a 'return_to' variable set it as a GET variable in case there;'s a form
-	//	validation error. Otherwise don't show it - cleaner.
-	
-	echo ( $return_to ) ? form_open( 'auth/login?return_to=' . $return_to, $attr ) : form_open( 'auth/login', $attr );
-	
+	//	validation error. Otherwise don't show it - cleaner. Using site_url() as the return_to variable
+	//	will most likely contain a full URL, causing form_open() not to site_url() it (resulting in the
+	//	form posting to auth/login rather than /auth/login).
+
+	echo $return_to? form_open( site_url( 'auth/login?return_to=' . $return_to, $attr ) ) : form_open( 'auth/login', $attr );
+
 	// --------------------------------------------------------------------------
-	
+
 	//	Write the HTML for the login form
 ?>
-	
+
 	<div class="row">
-	
+
 		<!--	LOGIN FIELDS	-->
 		<div class="first seven columns">
-		
+
 			<p>
 				<?php
 
@@ -63,14 +65,14 @@
 
 				?>
 			</p>
-		
+
 			<?php
-				
+
 				$_field			= 'email';
 				$_name			= lang( 'form_label_email' );
 				$_placeholder	= lang( 'auth_login_email_placeholder' );
 				$_error			= form_error( $_field ) ? 'error' : NULL
-			
+
 			?>
 			<div class="row <?=$_error?>">
 				<?=form_label( $_name, 'input-' . $_field, array( 'class' => 'two columns first' ) ); ?>
@@ -79,14 +81,14 @@
 					<?=form_error( $_field, '<div class="system-alert error no-close">', '</div>' )?>
 				</div>
 			</div>
-			
+
 			<?php
-				
+
 				$_field			= 'password';
 				$_name			= lang( 'form_label_password' );
 				$_placeholder	= lang( 'auth_login_pass_placeholder' );
 				$_error			= form_error( $_field ) ? 'error' : NULL
-			
+
 			?>
 			<div class="row <?=$_error?>">
 				<?=form_label( $_name, 'input-' . $_field, array( 'class' => 'two columns first' ) ); ?>
@@ -95,14 +97,14 @@
 					<?=form_error( $_field, '<div class="system-alert error no-close">', '</div>' )?>
 				</div>
 			</div>
-			
+
 			<!--	REMEMBER ME CHECKBOX	-->
 			<?php
-				
+
 				$_field	= 'remember';
 				$_name	= lang( 'auth_login_label_remember_me' );
 				$_error = form_error( $_field ) ? 'error' : NULL
-			
+
 			?>
 			<div class="row">
 				<label class="two columns first">&nbsp;</label>
@@ -113,8 +115,8 @@
 					</label>
 				</div>
 			</div>
-			
-			
+
+
 			<div class="row button-row">
 				<label class="two columns first">&nbsp;</label>
 				<div class="four columns last">
@@ -124,50 +126,50 @@
 					</small>
 				</div>
 			</div>
-		
+
 		</div>
-		
+
 		<!--	SOCIAL NETWORK BUTTONS	-->
 		<?php
-		
+
 			if ( module_is_enabled( 'auth[facebook]' ) || module_is_enabled( 'auth[facebook]' ) || module_is_enabled( 'auth[facebook]' ) ) :
-			
+
 				echo '<div class="eight columns last offset-by-one">';
 				echo '<p style="text-align:center;">' . lang( 'auth_login_social_message' ) . '</p>';
-				
+
 				// --------------------------------------------------------------------------
-				
+
 				//	FACEBOOK
 				if ( module_is_enabled( 'auth[facebook]' ) ) :
-				
+
 					echo '<p style="text-align:center;">' . anchor( 'auth/fb/connect', lang( 'auth_login_social_signin', 'Facebook' ), 'class="social-signin fb"' ) . '</p>';
-				
+
 				endif;
-				
+
 				//	TWITTER
 				if ( module_is_enabled( 'auth[twitter]' ) ) :
-				
+
 					echo '<p style="text-align:center;">' . anchor( 'auth/tw/connect', lang( 'auth_login_social_signin', 'Twitter' ), 'class="social-signin tw"' ) . '</p>';
-				
+
 				endif;
-				
+
 				//	LINKEDIN
 				if ( module_is_enabled( 'auth[linkedin]' ) ) :
-				
+
 					echo '<p style="text-align:center;">' . anchor( 'auth/li/connect', lang( 'auth_login_social_signin', 'LinkedIn' ), 'class="social-signin li"' ) . '</p>';
-				
+
 				endif;
-				
+
 				echo '</div>';
-			
+
 			endif;
-			
+
 		?>
 	</div>
-	
+
 <?php
-	
+
 	// --------------------------------------------------------------------------
-	
+
 	//	Close the form
 	echo form_close();
