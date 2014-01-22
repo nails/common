8a2852c16f6f2a8b536872d0050a1501b3ea4a3a
Pablo de la Pena <p@shedcollective.org>
2014-01-22 19:21:37 +0000

    Added array_unique_multi() to NAILS_array_helper.php

---
+++ helpers/NAILS_array_helper.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists( 'array_unique_multi' ) )
+{
+	function array_unique_multi( array $array )
+	{
+		//	Hat-tip: http://phpdevblog.niknovo.com/2009/01/using-array-unique-with-multidimensional-arrays.html
+
+		// Unique Array for return
+		$array_rewrite = array();
+
+		// Array with the md5 hashes
+		$array_hashes = array();
+
+		foreach ( $array AS $key => $item ) :
+
+			// Serialize the current element and create a md5 hash
+			$hash = md5( serialize( $item ) );
+
+			// If the md5 didn't come up yet, add the element to
+			// to array_rewrite, otherwise drop it
+
+			if ( ! isset( $array_hashes[$hash] ) ) :
+
+				// Save the current element hash
+				$array_hashes[$hash] = $hash;
+
+				// Add element to the unique Array
+				$array_rewrite[$key] = $item;
+
+			endif;
+
+		endif;
+
+		unset( $array_hashes );
+		unset( $key );
+		unset( $item );
+		unset( $hash );
+
+		return $array_rewrite;
+	}
+}
+
+
+/* End of file array_helper.php */
+/* Location: ./helpers/array_helper.php */
