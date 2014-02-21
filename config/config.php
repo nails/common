e682cdb301b06708ca9fe392e86352f65413eb96
Pablo de la Pena <p@shedcollective.org>
2014-02-21 17:29:25 +0000

    Adding `+` as an allowed URL character (no idea why CI hasn't already got it there)

--- config/config.php
+++ config/config.php
@@ -133,7 +133,7 @@ $config['subclass_prefix'] = 'NAILS_';
 | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
 |
 */
-$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-+';
 
 
 /*
