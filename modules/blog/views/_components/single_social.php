a818a6a3a972db5c3359c92d8231617f6a26bd81
Pablo de la Pena <p@shedcollective.org>
2014-04-17 14:17:16 +0100

    Bugfix: Typo in blog social tools

--- modules/blog/views/_components/single_social.php
+++ modules/blog/views/_components/single_social.php
@@ -21,7 +21,7 @@
 <hr />
 <div class="social-likes <?=$_layout?>" <?=$_counters?> data-url="<?=$post->url?>" data-single-title="<?=$_single_title?>" data-title="<?=$post->title?>">
 	<?=blog_setting( 'social_facebook_enabled' ) ? '<div class="facebook" title="Share link on Facebook">Facebook</div>' : '';?>
-	<?=blog_setting( 'social_ftwitter_enabled' ) ? '<div class="twitter" data-via="' . $_twitter_via . '" title="Share link on Twitter">Twitter</div>' : '';?>
+	<?=blog_setting( 'social_twitter_enabled' ) ? '<div class="twitter" data-via="' . $_twitter_via . '" title="Share link on Twitter">Twitter</div>' : '';?>
 	<?=blog_setting( 'social_googleplus_enabled' ) ? '<div class="plusone" title="Share link on Google+">Google+</div>' : '';?>
 	<?=blog_setting( 'social_pinterest_enabled' ) && $post->image_id ? '<div class="pinterest" data-media="' . cdn_serve( $post->image_id ) . '" title="Share image on Pinterest">Pinterest</div>' : '';?>
 </div>
