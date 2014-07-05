3f09a07cfd013fc93dab0d3d56b715f02b9e3cd9
Pablo de la Pena <p@shedcollective.org>
2014-07-06 00:52:39 +0100

    Added helper function for setting app settings

--- helpers/app_setting_helper.php
+++ helpers/app_setting_helper.php
@@ -23,3 +23,32 @@ if ( ! function_exists( 'app_setting' ) )
 		return get_instance()->app_setting_model->get( $key, $grouping, $force_refresh );
 	}
 }
+
+
+// --------------------------------------------------------------------------
+
+
+/**
+ * Helper for quickly setting app settings
+ *
+ * @access	public
+ * @param	none
+ * @return	void
+ */
+if ( ! function_exists( 'set_app_setting' ) )
+{
+	function set_app_setting( $key, $grouping = 'app', $value = NULL, $encrypt = FALSE )
+	{
+		//	Load the model if it's not already loaded
+		if ( ! get_instance()->load->model_is_loaded( 'app_setting_model' ) ) :
+
+			get_instance()->load->model( 'system/app_setting_model' );
+
+		endif;
+
+		// --------------------------------------------------------------------------
+
+		return get_instance()->app_setting_model->set( $key, $grouping, $value, $encrypt );
+	}
+}
+
