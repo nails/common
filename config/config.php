b9fbbd56dae206195f13e16772f2d819c637a870
Gary Duncan <g@shedcollective.org>
2014-11-12 10:55:59 +0000

    Removing IP Check for Sessions
    To avoid random timeouts on certain networks, we have decided to remove the IP check for sessions.

--- config/config.php
+++ config/config.php
@@ -262,7 +262,7 @@ $config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'nails_session';
-$config['sess_match_ip']		= TRUE;
+$config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= FALSE;
 $config['sess_time_to_update']	= 300;
 
