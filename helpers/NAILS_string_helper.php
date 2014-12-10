f77459753fa9f38c2f27f3c10852cfa13c7c076f
Pablo de la Pena <p@shedcollective.org>
2014-12-10 11:50:29 +0000

    Added string helpers for transforming strings to/from camelcase/underscore

--- helpers/NAILS_string_helper.php
+++ helpers/NAILS_string_helper.php
@@ -1,34 +1,58 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-
-/**
- * str_lreplace
- *
- * Replace the last occurance of a string within a string with a string
- *
- * @access	public
- * @param	string	The item to replace
- * @param	string	The string to replace with
- * @param	string	The string to search
- * @return	string
- */
-if ( ! function_exists('str_lreplace'))
+if (!function_exists('str_lreplace'))
 {
-	function str_lreplace( $search, $replace, $subject )
-	{
-		$pos = strrpos( $subject, $search );
-		
-		if( $pos !== FALSE ) :
-		
-			$subject = substr_replace( $subject, $replace, $pos, strlen( $search ) );
-			
-		endif;
-		
-		return $subject;
-	}
+    /**
+     * Replace the last occurance of a string within a string with a string
+     * @param  string $search  The substring to replace
+     * @param  string $replace The string to replace the substring with
+     * @param  string $subject The string to search
+     * @return string
+     */
+    function str_lreplace($search, $replace, $subject)
+    {
+        $pos = strrpos($subject, $search);
+
+        if ($pos !== false) {
+
+            $subject = substr_replace($subject, $replace, $pos, strlen($search));
+        }
+
+        return $subject;
+    }
 }
 
+// --------------------------------------------------------------------------
 
+if (!function_exists('underscore_to_camelcase'))
+{
+    /**
+     * Transforms a string with underscores into a camelcased string
+     * @param  string  $str     The string to transform
+     * @param  boolean $lcfirst Whether or not to lowercase the first letter of the transformed string or not
+     * @return string
+     */
+    function underscore_to_camelcase($str, $lcfirst = true)
+    {
+        $str = explode('_', $str);
+        $str = array_map('ucfirst', $str);
+        $str = implode($str);
+        $str = $lcfirst ? lcfirst($str) : $str;
+        return $str;
+    }
+}
 
-/* End of file NAILS_string_helper.php */
-/* Location: ./helpers/NAILS_string_helper.php */
+// --------------------------------------------------------------------------
+
+if (!function_exists('camelcase_to_underscore'))
+{
+    /**
+     * Transforms a camelcased string to underscores
+     * @param  string $str The string to transform
+     * @return string
+     */
+    function camelcase_to_underscore($str)
+    {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $str));
+    }
+}
