e00a4c3de1d564cea291c3ad719a66fd2a4337c4
Pablo de la Pena <p@shedcollective.org>
2015-01-27 15:13:22 +0000

    Minor style fix and documentation

--- helpers/event_helper.php
+++ helpers/event_helper.php
@@ -2,13 +2,19 @@
 
 if (!function_exists('create_event')) {
 
-    function create_event($type, $vars = null, $created_by = null, $ref = null, $_recorded = null)
+    /**
+     * Creates an event object
+     * @param  string  $type      The type of event to create
+     * @param  mixed   $data      Any data to store alongside the event object
+     * @param  integer $createdBy The event creator (null == system)
+     * @param  integer $ref       A numeric reference to store alongside the event (e.g the id of the object the event relates to)
+     * @param  string  $recorded  A strtotime() friendly string of the date to use instead of NOW() for the created date
+     * @return mixed              Int on success false on failure
+     */
+    function create_event($type, $data = null, $createdBy = null, $ref = null, $recorded = null)
     {
-        $_ci =& get_instance();
-        $_ci->load->library('event');
-        return $_ci->event->create($type, $vars, $created_by, $ref);
+        $ci =& get_instance();
+        $ci->load->library('event');
+        return $ci->event->create($type, $data, $createdBy, $ref, $recorded);
     }
 }
-
-/* End of file event_helper.php */
-/* Location: ./helpers/event_helper.php */
