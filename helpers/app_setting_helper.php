622288568104b834d79a47f1a3e616a5bc1b9387
Pablo de la Pena <p@shedcollective.org>
2015-02-18 12:09:49 +0000

    Tidy up

--- helpers/app_setting_helper.php
+++ helpers/app_setting_helper.php
@@ -6,11 +6,11 @@ if (!function_exists('app_setting')) {
      * Helper for quickly accessing app settings
      * @return  mixed
      */
-	function app_setting($key = null, $grouping = 'app', $force_refresh = false)
-	{
+    function app_setting($key = null, $grouping = 'app', $force_refresh = false)
+    {
         get_instance()->load->model('app_setting_model');
-		return get_instance()->app_setting_model->get($key, $grouping, $force_refresh);
-	}
+        return get_instance()->app_setting_model->get($key, $grouping, $force_refresh);
+    }
 }
 
 // --------------------------------------------------------------------------
@@ -21,10 +21,10 @@ if (!function_exists('set_app_setting')) {
      * Helper for quickly setting app settings
      * @return  void
      */
-	function set_app_setting($key, $grouping = 'app', $value = null, $encrypt = false)
-	{
+    function set_app_setting($key, $grouping = 'app', $value = null, $encrypt = false)
+    {
         get_instance()->load->model('app_setting_model');
-		return get_instance()->app_setting_model->set($key, $grouping, $value, $encrypt);
-	}
+        return get_instance()->app_setting_model->set($key, $grouping, $value, $encrypt);
+    }
 }
 
