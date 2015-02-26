cb3544d3f9c5cf6b1f35b8b0e49ccb4e885214e3
Pablo de la Pena <p@shedcollective.org>
2015-02-26 23:12:58 +0000

    Tidying

--- config/app_notifications.php
+++ config/app_notifications.php
@@ -12,17 +12,17 @@
 
 $config['notification_definitions'] = array();
 
-$config['notification_definitions']['app']				= new stdClass();
-$config['notification_definitions']['app']->slug		= 'app';
-$config['notification_definitions']['app']->label		= 'Site';
-$config['notification_definitions']['app']->description	= 'General site notifications.';
-$config['notification_definitions']['app']->options		= array();
+$config['notification_definitions']['app']              = new stdClass();
+$config['notification_definitions']['app']->slug        = 'app';
+$config['notification_definitions']['app']->label       = 'Site';
+$config['notification_definitions']['app']->description = 'General site notifications.';
+$config['notification_definitions']['app']->options     = array();
 
-$config['notification_definitions']['app']->options['default']					= new stdClass();
-$config['notification_definitions']['app']->options['default']->slug			= 'default';
-$config['notification_definitions']['app']->options['default']->label			= 'Generic';
-$config['notification_definitions']['app']->options['default']->sub_label		= '';
-$config['notification_definitions']['app']->options['default']->tip				= '';
-$config['notification_definitions']['app']->options['default']->email_subject	= '';
-$config['notification_definitions']['app']->options['default']->email_tpl		= '';
-$config['notification_definitions']['app']->options['default']->email_message	= '';
+$config['notification_definitions']['app']->options['default']                = new stdClass();
+$config['notification_definitions']['app']->options['default']->slug          = 'default';
+$config['notification_definitions']['app']->options['default']->label         = 'Generic';
+$config['notification_definitions']['app']->options['default']->sub_label     = '';
+$config['notification_definitions']['app']->options['default']->tip           = '';
+$config['notification_definitions']['app']->options['default']->email_subject = '';
+$config['notification_definitions']['app']->options['default']->email_tpl     = '';
+$config['notification_definitions']['app']->options['default']->email_message = '';
