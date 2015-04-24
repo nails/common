129b37e4ddb22f4b85807bb4a8d375c464eb8a65
Pablo de la Peña <p@shedcollective.org>
2015-04-24 20:26:52 +0100

    Tidying up shop_helper

--- helpers/shop_helper.php
+++ helpers/shop_helper.php
@@ -1,73 +1,53 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-/**
- * Returns the content of the basket
- *
- * @access	public
- * @param	none
- * @return	object
- */
-if ( ! function_exists( 'get_basket' ) )
-{
-	function get_basket()
-	{
-		get_instance()->load->model( 'shop/shop_model' );
-		get_instance()->load->model( 'shop/shop_basket_model' );
+if (!function_exists('getBasket')) {
 
-		// --------------------------------------------------------------------------
+    /**
+     * Returns the contents of the basket
+     * @return stdClass
+     */
+    function getBasket()
+    {
+        get_instance()->load->model('shop/shop_model');
+        get_instance()->load->model('shop/shop_basket_model');
 
-		return get_instance()->shop_basket_model->get();
-	}
+        return get_instance()->shop_basket_model->get();
+    }
 }
-
 
 // --------------------------------------------------------------------------
 
+if (!function_exists('getBasketCount')) {
 
-/**
- * Gets the number of items of the basket
- *
- * @access	public
- * @param	none
- * @return	void
- */
-if ( ! function_exists( 'get_basket_count' ) )
-{
-	function get_basket_count( $respect_quantity = TRUE )
-	{
-		get_instance()->load->model( 'shop/shop_model' );
-		get_instance()->load->model( 'shop/shop_basket_model' );
+    /**
+     * Returns the number of items in the baset
+     * @param  boolean $respectQuantity Whether to respect the item quantities
+     * @return int
+     */
+    function getBasketCount($respectQuantity = true)
+    {
+        get_instance()->load->model('shop/shop_model');
+        get_instance()->load->model('shop/shop_basket_model');
 
-		// --------------------------------------------------------------------------
-
-		return get_instance()->shop_basket_model->get_count( $respect_quantity );
-	}
+        return get_instance()->shop_basket_model->getCount($respectQuantity);
+    }
 }
-
 
 // --------------------------------------------------------------------------
 
+if (!function_exists('getBasketTotal')) {
 
-/**
- * Gets the current basket total
- *
- * @access	public
- * @param	none
- * @return	void
- */
-if ( ! function_exists( 'get_basket_total' ) )
-{
-	function get_basket_total( $include_symbol = FALSE, $include_thousands = FALSE )
-	{
-		get_instance()->load->model( 'shop/shop_model' );
-		get_instance()->load->model( 'shop/shop_basket_model' );
+    /**
+     * Returns the basket quantity
+     * @param  boolean $includeSymbol    Whether to include the currency symbol
+     * @param  boolean $includeThousands Whether to mark the thousands
+     * @return string
+     */
+    function getBasketTotal($includeSymbol = false, $includeThousands = false)
+    {
+        get_instance()->load->model('shop/shop_model');
+        get_instance()->load->model('shop/shop_basket_model');
 
-		// --------------------------------------------------------------------------
-
-		return get_instance()->shop_basket_model->get_total( $include_symbol, $include_thousands );
-	}
+        return get_instance()->shop_basket_model->getTotal($includeSymbol, $includeThousands);
+    }
 }
-
-
-/* End of file shop_helper.php */
-/* Location: ./modules/shop/helpers/shop_helper.php */
