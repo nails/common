dcf8b10bef6eb6aa3acb32f1c7e28f72f77f76db
Pablo de la Peña <p@shedcollective.org>
2015-09-09 00:50:32 +0100

    Added Meta library + tests
    - Added Meta library for adding <meta> tags to pages
    - Loading Meta library by default in CORE_NAILS_Controller
    - Added tests for Meta library using PHPUnit (hooray)
    - Corrected a few typos / layout issues
    - Removed unecessary 404 view
    - Updated blank.php to use the Meta library

---
+++ tests/Library/MetaTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Nails\Common;
+
+class MetaTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \Nails\Common\Meta::getEntries
+     */
+    public function testGetEntries()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $this->assertCount(0, $oMeta->getEntries());
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::addRaw
+     */
+    public function testAddRaw()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $aData = array(
+            'foo' => 'bar'
+        );
+
+        $oMeta->addRaw($aData);
+        $this->assertCount(1, $oMeta->getEntries());
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::addRaw
+     */
+    public function testAddRawDoesRemovesDupliates()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $aData = array(
+            'foo' => 'bar'
+        );
+
+        $oMeta->addRaw($aData);
+        $oMeta->addRaw($aData);
+        $this->assertCount(1, $oMeta->getEntries());
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::addRaw
+     */
+    public function testAddRawIsChainable()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $aData = array(
+            'foo' => 'bar'
+        );
+
+        $this->assertInstanceOf('Nails\Common\Library\Meta', $oMeta->addRaw($aData));
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::removeRaw
+     */
+    public function testRemoveRaw()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $aData = array(
+            'foo' => 'bar'
+        );
+
+        $oMeta->addRaw($aData);
+        $oMeta->removeRaw($aData);
+        $this->assertCount(0, $oMeta->getEntries());
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::add
+     */
+    public function testAdd()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $oMeta->add('foo', 'bar');
+        $this->assertEquals(1, count($oMeta->getEntries()));
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::addRaw
+     */
+    public function testAddIsChainable()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $this->assertInstanceOf('Nails\Common\Library\Meta', $oMeta->add('foo', 'bar'));
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::remove
+     */
+    public function testRemove()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+        $oMeta->add('foo', 'bar');
+        $oMeta->remove('foo', 'bar');
+        $this->assertCount(0, $oMeta->getEntries());
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::outputAr
+     */
+    public function testOutputAr()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+
+        $oMeta->add('foo', 'bar');
+        $oMeta->add('cat', 'dog');
+
+        $aExpected = array(
+            '<meta name="foo" content="bar" />',
+            '<meta name="cat" content="dog" />'
+        );
+
+        $this->assertEquals($aExpected, $oMeta->outputAr());
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \Nails\Common\Meta::outputStr
+     */
+    public function testOutputStr()
+    {
+        $oMeta = new \Nails\Common\Library\Meta();
+
+        $oMeta->add('foo', 'bar');
+        $oMeta->add('cat', 'dog');
+
+        $aExpected = '<meta name="foo" content="bar" />' . "\n" . '<meta name="cat" content="dog" />';
+
+        $this->assertEquals($aExpected, $oMeta->outputStr());
+    }
+}
