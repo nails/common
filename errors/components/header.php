a14bf34e27becda79513236fa28d1ced838c3804
Pablo de la Peña <p@shedcollective.org>
2015-10-13 12:26:56 +0100

    Better handling of Nails branding

--- errors/components/header.php
+++ errors/components/header.php
@@ -1,61 +1,62 @@
-<h1>
-    <?php
+<?php
 
-    if (!isset($paths)) {
+if (!isset($paths)) {
 
-        $paths = array();
+    $paths = array();
+}
+
+if (defined('FCPATH') && defined('BASE_URL')) {
+
+    $paths[] = array(
+        FCPATH . 'assets/img/logo.png',
+        BASE_URL . 'assets/img/logo.png'
+    );
+
+    $paths[] = array(
+        FCPATH . 'assets/img/logo.jpg',
+        BASE_URL . 'assets/img/logo.jpg'
+    );
+
+    $paths[] = array(
+        FCPATH . 'assets/img/logo.gif',
+        BASE_URL . 'assets/img/logo.gif'
+    );
+
+    $paths[] = array(
+        FCPATH . 'assets/img/logo/logo.png',
+        BASE_URL . 'assets/img/logo/logo.png'
+    );
+
+    $paths[] = array(
+        FCPATH . 'assets/img/logo/logo.jpg',
+        BASE_URL . 'assets/img/logo/logo.jpg'
+    );
+
+    $paths[] = array(
+        FCPATH . 'assets/img/logo/logo.gif',
+        BASE_URL . 'assets/img/logo/logo.gif'
+    );
+
+    if (NAILS_BRANDING) {
+
+        $paths[] = array(
+            FCPATH . 'vendor/nailsapp/module-asset/assets/img/nails/icon/icon@2x.png',
+            BASE_URL . 'vendor/nailsapp/module-asset/assets/img/nails/icon/icon@2x.png'
+        );
     }
+}
 
-    if (defined('FCPATH') && defined('BASE_URL')) {
+foreach ($paths as $path) {
 
-        $paths[] = array(
-            FCPATH . 'assets/img/logo.png',
-            BASE_URL . 'assets/img/logo.png'
-        );
+    if (is_file($path[0])) {
 
-        $paths[] = array(
-            FCPATH . 'assets/img/logo.jpg',
-            BASE_URL . 'assets/img/logo.jpg'
-        );
-
-        $paths[] = array(
-            FCPATH . 'assets/img/logo.gif',
-            BASE_URL . 'assets/img/logo.gif'
-        );
-
-        $paths[] = array(
-            FCPATH . 'assets/img/logo/logo.png',
-            BASE_URL . 'assets/img/logo/logo.png'
-        );
-
-        $paths[] = array(
-            FCPATH . 'assets/img/logo/logo.jpg',
-            BASE_URL . 'assets/img/logo/logo.jpg'
-        );
-
-        $paths[] = array(
-            FCPATH . 'assets/img/logo/logo.gif',
-            BASE_URL . 'assets/img/logo/logo.gif'
-        );
-
-        if (NAILS_BRANDING) {
-
-            $paths[] = array(
-                FCPATH . 'vendor/nailsapp/module-asset/assets/img/nails/icon/icon@2x.png',
-                BASE_URL . 'vendor/nailsapp/module-asset/assets/img/nails/icon/icon@2x.png'
-            );
-        }
+        ?>
+        <h1>
+            <div id="logoContainer">
+                <img src="<?=$path[1]?>" id="logo" />
+            </div>
+        </h1>
+        <?php
+        break;
     }
-
-    foreach ($paths as $path) {
-
-        if (is_file($path[0])) {
-
-            echo '<div id="logoContainer">';
-                echo '<img src="' . $path[1] . '" id="logo" />';
-            echo '</div>';
-            break;
-        }
-    }
-    ?>
-</h1>
+}
