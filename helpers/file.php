1a9f39d7f2452c7ced79bb36e6079139deec4135
Pablo de la Peña <p@shedcollective.org>
2016-04-01 11:13:05 +0100

    Moved helper to file helper

--- helpers/file.php
+++ helpers/file.php
@@ -10,4 +10,45 @@
  * @link
  */
 
+if (!function_exists('readFileChunked')) {
+
+    /**
+     * Outputs a file in bytesized chunks.
+     * http://teddy.fr/2007/11/28/how-serve-big-files-through-php/
+     * @param  string  $filename  The file to output
+     * @param  integer $chunkSize The chunk size, in bytes
+     * @return mixed              Ineger on success, false on failure
+     */
+    function readFileChunked($filename, $chunkSize = 1048576)
+    {
+        $bytesRead = 0;
+
+        // $handle = fopen($filename, "rb");
+        $handle = fopen($filename, 'rb');
+        if ($handle === false) {
+
+            return false;
+        }
+
+        while (!feof($handle)) {
+
+            $buffer = fread($handle, $chunkSize);
+            echo $buffer;
+
+            $bytesRead += strlen($buffer);
+        }
+
+        $status = fclose($handle);
+
+        if ($status) {
+
+            return $bytesRead;
+
+        } else {
+
+            return false;
+        }
+    }
+}
+
 include FCPATH . 'vendor/rogeriopradoj/codeigniter/system/helpers/file_helper.php';
