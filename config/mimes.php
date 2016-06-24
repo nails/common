a1022b2358bd9dc69fdbf7dccfc691b38ff4f16c
Carl Wood <c@shedcollective.org>
2016-06-24 11:28:12 +0100

    Add ogv and webm mime types

--- config/mimes.php
+++ config/mimes.php
@@ -9,6 +9,8 @@ require FCPATH . 'vendor/rogeriopradoj/codeigniter/application/config/mimes.php'
 
 //  Video
 $mimes['mp4'] = 'video/mp4';
+$mimes['ogv'] = 'video/ogg';
+$mimes['webm'] = 'video/webm';
 
 //  MS Office
 //  http://stackoverflow.com/a/4212908/789224
