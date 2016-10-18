3e901c7740979048ca7ac4c8358c3d5ea990649d
Carl Wood <c@shedcollective.org>
2016-10-18 16:43:16 +0100

    Use  and , and add body class functionality

--- views/structure/header/blank.php
+++ views/structure/header/blank.php
@@ -1,3 +1,23 @@
+<?php
+
+use Nails\Factory;
+
+$aPageTitle = [];
+if (!empty($page->seo->title)) {
+
+    $aPageTitle[] = $page->seo->title;
+
+} elseif (!empty($page->title)) {
+
+    $aPageTitle[] = $page->title;
+}
+
+$aPageTitle[] = APP_NAME;
+
+
+$sBodyClass = !empty($page->body_class) ? $page->body_class : '';
+
+?>
 <!DOCTYPE html>
 <!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
@@ -5,31 +25,22 @@
         <?php
 
         echo '<title>';
-
-        if (!empty($page->seo->title)) {
-
-            echo $page->seo->title . ' - ';
-
-        } elseif (!empty($page->title)) {
-
-            echo $page->title . ' - ';
-        }
-
-        echo APP_NAME;
-
+        echo implode(' - ', $aPageTitle);
         echo '</title>';
 
         // --------------------------------------------------------------------------
 
         //  Meta tags
-        echo $this->meta->outputStr();
+        $oMeta = Factory::service('Meta');
+        echo $oMeta->outputStr();
 
         // --------------------------------------------------------------------------
 
         //  Assets
-        $this->asset->output('CSS');
-        $this->asset->output('CSS-INLINE');
-        $this->asset->output('JS-INLINE-HEADER');
+        $oAsset = Factory::service('Meta');
+        $oAsset->output('CSS');
+        $oAsset->output('CSS-INLINE');
+        $oAsset->output('JS-INLINE-HEADER');
 
         ?>
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -39,4 +50,4 @@
           <script src="<?=NAILS_ASSETS_URL . 'bower_components/respond/dest/respond.min.js'?>"></script>
         <![endif]-->
     </head>
-    <body>
+    <body <?=$sBodyClass ? 'class="' . $sBodyClass . '"' : ''?>>
