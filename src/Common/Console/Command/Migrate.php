e396c94cdc9b08bb6cfdcad15acb12d4cf4dd651
Pablo de la Peña <p@shedcollective.org>
2017-01-27 17:09:37 +0000

    Using the new `BaseAlias` base class

--- src/Common/Console/Command/Migrate.php
+++ src/Common/Console/Command/Migrate.php
@@ -2,33 +2,10 @@
 
 namespace Nails\Common\Console\Command;
 
-use Nails\Console\Command\Base;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Nails\Console\Command\BaseAlias;
 
-class Migrate extends Base
+class Migrate extends BaseAlias
 {
-    /**
-     * Configures the app
-     * @return void
-     */
-    protected function configure()
-    {
-        $this->setName('migrate');
-        $this->setDescription('Alias to db:migrate');
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Executes the app
-     * @param  InputInterface $oInput The Input Interface provided by Symfony
-     * @param  OutputInterface $oOutput The Output Interface provided by Symfony
-     * @return int
-     */
-    protected function execute(InputInterface $oInput, OutputInterface $oOutput)
-    {
-        parent::execute($oInput, $oOutput);
-        return $this->callCommand('db:migrate');
-    }
+    const COMMAND = 'db:migrate';
+    const ALIAS   = 'migrate';
 }
