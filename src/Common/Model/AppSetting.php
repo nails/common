0d7f4b23b3004b3ff326e795311edd22be0294e0
Pablo de la Peña <p@shedcollective.org>
2017-01-30 10:10:35 +0000

    Calling services as they’re needed
    So model can be laoded in console without overheads/errors

--- src/Common/Model/AppSetting.php
+++ src/Common/Model/AppSetting.php
@@ -12,18 +12,17 @@
 
 namespace Nails\Common\Model;
 
+use Nails\Common\Traits\ErrorHandling;
 use Nails\Factory;
 
 class AppSetting
 {
-    use \Nails\Common\Traits\ErrorHandling;
+    use ErrorHandling;
 
     // --------------------------------------------------------------------------
 
     protected $sTable;
     protected $aSettings;
-    protected $oDb;
-    protected $oEncrypt;
 
     // --------------------------------------------------------------------------
 
@@ -33,17 +32,15 @@ class AppSetting
     public function __construct()
     {
         $this->sTable    = NAILS_DB_PREFIX . 'app_setting';
-        $this->aSettings = array();
-        $this->oDb       = Factory::service('Database');
-        $this->oEncrypt  = Factory::service('Encrypt');
+        $this->aSettings = [];
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Gets settings associated with a particular group/key
-     * @param  string  $sKey          The key to retrieve
-     * @param  string  $sGrouping     The group the key belongs to
+     * @param  string $sKey The key to retrieve
+     * @param  string $sGrouping The group the key belongs to
      * @param  boolean $bForceRefresh Whether to force a group refresh
      * @return array
      */
@@ -51,17 +48,19 @@ class AppSetting
     {
         if (!isset($this->aSettings[$sGrouping]) || $bForceRefresh) {
 
-            $this->oDb->where('grouping', $sGrouping);
-            $aSettings = $this->oDb->get($this->sTable)->result();
+            $oDb = Factory::service('Database');
+            $oDb->where('grouping', $sGrouping);
+            $aSettings = $oDb->get($this->sTable)->result();
 
-            $this->aSettings[$sGrouping] = array();
+            $this->aSettings[$sGrouping] = [];
 
             foreach ($aSettings as $oSetting) {
 
                 $sValue = $oSetting->value;
 
                 if (!empty($oSetting->is_encrypted)) {
-                    $sValue = $this->oEncrypt->decode($sValue, APP_PRIVATE_KEY);
+                    $oEncrypt = Factory::service('Encrypt');
+                    $sValue   = $oEncrypt->decode($sValue, APP_PRIVATE_KEY);
                 }
 
                 $this->aSettings[$sGrouping][$oSetting->key] = json_decode($sValue);
@@ -85,15 +84,16 @@ class AppSetting
     /**
      * Set a group/key either by passing an array of key=>value pairs as the $key
      * or by passing a string to $key and setting $value
-     * @param  mixed   $mKey      The key to set, or an array of key => value pairs
-     * @param  string  $sGrouping The grouping to store the keys under
-     * @param  mixed   $mValue    The data to store, only used if $mKey is a string
-     * @param  boolean $bEncrypt  Whether to encrypt the data or not
+     * @param  mixed $mKey The key to set, or an array of key => value pairs
+     * @param  string $sGrouping The grouping to store the keys under
+     * @param  mixed $mValue The data to store, only used if $mKey is a string
+     * @param  boolean $bEncrypt Whether to encrypt the data or not
      * @return boolean
      */
     public function set($mKey, $sGrouping = 'app', $mValue = null, $bEncrypt = false)
     {
-        $this->oDb->trans_begin();
+        $oDb = Factory::service('Database');
+        $oDb->trans_begin();
 
         if (is_array($mKey)) {
 
@@ -107,14 +107,16 @@ class AppSetting
             $this->doSet($mKey, $sGrouping, $mValue, $bEncrypt);
         }
 
-        if ($this->oDb->trans_status() === false) {
+        if ($oDb->trans_status() === false) {
 
-            $this->oDb->trans_rollback();
+            $oDb->trans_rollback();
+
             return false;
 
         } else {
 
-            $this->oDb->trans_commit();
+            $oDb->trans_commit();
+
             return true;
         }
     }
@@ -123,10 +125,10 @@ class AppSetting
 
     /**
      * Inserts/Updates a group/key value
-     * @param string  $sKey      The key to set
-     * @param string  $sGrouping The key's grouping
-     * @param mixed   $mValue    The value of the group/key
-     * @param boolean $bEncrypt  Whether to encrypt the data or not
+     * @param string $sKey The key to set
+     * @param string $sGrouping The key's grouping
+     * @param mixed $mValue The value of the group/key
+     * @param boolean $bEncrypt Whether to encrypt the data or not
      * @return void
      */
     protected function doSet($sKey, $sGrouping, $mValue, $bEncrypt)
@@ -135,27 +137,29 @@ class AppSetting
         $bEncrypt = (bool) $bEncrypt;
 
         if ($bEncrypt) {
-            $sValue = $this->oEncrypt->encode($sValue, APP_PRIVATE_KEY);
+            $oEncrypt = Factory::service('Encrypt');
+            $sValue   = $oEncrypt->encode($sValue, APP_PRIVATE_KEY);
         }
 
-        $this->oDb->where('key', $sKey);
-        $this->oDb->where('grouping', $sGrouping);
+        $oDb = Factory::service('Database');
+        $oDb->where('key', $sKey);
+        $oDb->where('grouping', $sGrouping);
 
-        if ($this->oDb->count_all_results($this->sTable)) {
+        if ($oDb->count_all_results($this->sTable)) {
 
-            $this->oDb->set('value', $sValue);
-            $this->oDb->set('is_encrypted', $bEncrypt);
-            $this->oDb->where('grouping', $sGrouping);
-            $this->oDb->where('key', $sKey);
-            $this->oDb->update($this->sTable);
+            $oDb->set('value', $sValue);
+            $oDb->set('is_encrypted', $bEncrypt);
+            $oDb->where('grouping', $sGrouping);
+            $oDb->where('key', $sKey);
+            $oDb->update($this->sTable);
 
         } else {
 
-            $this->oDb->set('value', $sValue);
-            $this->oDb->set('grouping', $sGrouping);
-            $this->oDb->set('key', $sKey);
-            $this->oDb->set('is_encrypted', $bEncrypt);
-            $this->oDb->insert($this->sTable);
+            $oDb->set('value', $sValue);
+            $oDb->set('grouping', $sGrouping);
+            $oDb->set('key', $sKey);
+            $oDb->set('is_encrypted', $bEncrypt);
+            $oDb->insert($this->sTable);
         }
     }
 
@@ -163,13 +167,14 @@ class AppSetting
 
     /**
      * Deletes a key for a particular group
-     * @param mixed   $mKey      The key to delete
-     * @param string  $sGrouping The key's grouping
+     * @param mixed $mKey The key to delete
+     * @param string $sGrouping The key's grouping
      * @return bool
      */
     public function delete($mKey, $sGrouping)
     {
-        $this->oDb->trans_begin();
+        $oDb = Factory::service('Database');
+        $oDb->trans_begin();
 
         if (is_array($mKey)) {
 
@@ -182,14 +187,16 @@ class AppSetting
             $this->doDelete($mKey, $sGrouping);
         }
 
-        if ($this->oDb->trans_status() === false) {
+        if ($oDb->trans_status() === false) {
 
-            $this->oDb->trans_rollback();
+            $oDb->trans_rollback();
+
             return false;
 
         } else {
 
-            $this->oDb->trans_commit();
+            $oDb->trans_commit();
+
             return true;
         }
     }
@@ -198,15 +205,17 @@ class AppSetting
 
     /**
      * Actually performs the deletion of the row.
-     * @param  string $sKey      The key to delete
+     * @param  string $sKey The key to delete
      * @param  string $sGrouping They key's grouping
      * @return bool
      */
     protected function doDelete($sKey, $sGrouping)
     {
-        $this->oDb->where('key', $sKey);
-        $this->oDb->where('grouping', $sGrouping);
-        return $this->oDb->delete($this->sTable);
+        $oDb = Factory::service('Database');
+        $oDb->where('key', $sKey);
+        $oDb->where('grouping', $sGrouping);
+
+        return $oDb->delete($this->sTable);
     }
 
     // --------------------------------------------------------------------------
@@ -218,7 +227,20 @@ class AppSetting
      */
     public function deleteGroup($sGrouping)
     {
-        $this->oDb->where('grouping', $sGrouping);
-        return $this->oDb->delete($this->sTable);
+        $oDb = Factory::service('Database');
+        $oDb->where('grouping', $sGrouping);
+
+        return $oDb->delete($this->sTable);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the table name used by app settings
+     * @return string
+     */
+    public function getTableName()
+    {
+        return $this->sTable;
     }
 }
