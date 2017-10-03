e741f021a061889ad9d7442b1108414c7a20d879
Ben Hurrell <ben@shedcollective.org>
2017-10-03 14:56:12 +0100

    Added proxy check for CI runtime before loading localisation

--- core/CORE_NAILS_Lang.php
+++ core/CORE_NAILS_Lang.php
@@ -62,6 +62,14 @@ class CORE_NAILS_Lang extends MX_Lang
      */
     public function load($langfile, $lang = '', $return = false, $add_suffix = true, $alt_path = '', $_module = '')
     {
+        // Proxy check to determine whether runtime was initiated through CodeIgniter
+        if (!class_exists('CI')) {
+
+            // Check failed; abandon this attempt to utilise CodeIgniter templating/localisation constructs
+                // => a null/void return type is consistent with behaviour in the CI_Lang base class
+            return;
+        }
+
         //  Are we loading an array of languages? If so, handle each one on its own.
         if (is_array($langfile)) {
 
