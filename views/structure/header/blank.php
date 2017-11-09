e52cb59e8b199fa14865312155939e1fdf4579fd
Pablo de la Peña <p@shedcollective.org>
2017-11-09 19:20:16 +0000

    Added ability to specify page language and add classes to the <html> tag

--- views/structure/header/blank.php
+++ views/structure/header/blank.php
@@ -11,6 +11,19 @@ if (!empty($page->seo->title)) {
 
 $aPageTitle[] = APP_NAME;
 
+if (!empty($page->html_lang)) {
+    $sHtmlLang = $page->html_lang;
+} else {
+    $sHtmlLang = 'en';
+}
+
+if (!empty($page->html_class) && is_array($page->html_class)) {
+    $aHtmlClass = $page->html_class;
+} elseif (!empty($page->html_class)) {
+    $aHtmlClass = [$page->html_class];
+} else {
+    $aHtmlClass = [];
+}
 
 if (!empty($page->body_class) && is_array($page->body_class)) {
     $aBodyClass = $page->body_class;
@@ -22,8 +35,10 @@ if (!empty($page->body_class) && is_array($page->body_class)) {
 
 ?>
 <!DOCTYPE html>
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<!--[if IE 8 ]>
+<html class="ie ie8 <?=implode(' ', $aHtmlClass)?>" lang="<?=$sHtmlLang?>>"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="<?=implode(' ', $aHtmlClass)?>" lang="<?=$sHtmlLang?>"> <!--<![endif]-->
     <head>
         <?php
 
@@ -49,8 +64,8 @@ if (!empty($page->body_class) && is_array($page->body_class)) {
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-          <script src="<?=NAILS_ASSETS_URL . 'bower_components/html5shiv/dist/html5shiv.js'?>"></script>
-          <script src="<?=NAILS_ASSETS_URL . 'bower_components/respond/dest/respond.min.js'?>"></script>
+        <script src="<?=NAILS_ASSETS_URL . 'bower_components/html5shiv/dist/html5shiv.js'?>"></script>
+        <script src="<?=NAILS_ASSETS_URL . 'bower_components/respond/dest/respond.min.js'?>"></script>
         <![endif]-->
     </head>
-    <body<?=$aBodyClass ? ' class="' . implode(' ', $aBodyClass) . '"' : ''?>>
+    <body class="<?=implode(' ', $aBodyClass)?>">
