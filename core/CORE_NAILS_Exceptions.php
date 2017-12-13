a4805b375a2115ac69ba94d3f6ca5ee322f38260
Pablo de la Peña <p@shedcollective.org>
2017-12-13 16:55:44 +0000

    Improved 404 handling

--- core/CORE_NAILS_Exceptions.php
+++ core/CORE_NAILS_Exceptions.php
@@ -15,39 +15,47 @@ use Nails\Common\Exception\NailsException;
 
 class CORE_NAILS_Exceptions extends CI_Exceptions
 {
-    private $error_has_occurred = false;
-    private $recent_errors      = array();
+    private $bErrorHasOccurred = false;
+    private $aRecentErrors     = [];
 
     // --------------------------------------------------------------------------
 
     /**
      * Override the show_error method in order to throw an exception rather than display a screen.
-     * @param  string $heading     Unused
-     * @param  string $message     The exception message (can be an array)
-     * @param  string $template    Unused
-     * @param  int    $status_code The exception number
+     *
+     * @param string  $sHeading      The error heading
+     * @param string  $sMessage      The exception message (can be an array)
+     * @param string  $sTemplate     The template to use
+     * @param int     $iStatusCode   The exception number
+     * @param boolean $bUseException Whether to use an exception
+     *
      * @throws \Nails\Common\Exception\NailsException
      * @return void
      */
-    public function show_error($heading, $message, $template = 'error_general', $status_code = 500, $use_exception = false)
-    {
-        if (is_array($message)) {
-            $message = implode($message, ' ');
+    public function show_error(
+        $sHeading,
+        $sMessage,
+        $sTemplate = 'error_general',
+        $iStatusCode = 500,
+        $bUseException = false
+    ) {
+        if (is_array($sMessage)) {
+            $sMessage = implode($sMessage, ' ');
         }
 
-        if ($use_exception) {
-            throw new NailsException($message, $status_code);
+        if ($bUseException) {
+            throw new NailsException($sMessage, $iStatusCode);
         } else {
 
-            $sTemplate = APPPATH.'errors/' . $template . '.php';
+            $sTemplate = APPPATH . 'errors/' . $sTemplate . '.php';
             if (!file_exists($sTemplate)) {
-                _NAILS_ERROR($message, $heading);
+                _NAILS_ERROR($sMessage, $sHeading);
             } else {
                 ob_start();
                 include($sTemplate);
-                $buffer = ob_get_contents();
+                $sBuffer = ob_get_contents();
                 ob_end_clean();
-                echo $buffer;
+                echo $sBuffer;
             }
 
             exit;
@@ -64,32 +72,26 @@ class CORE_NAILS_Exceptions extends CI_Exceptions
      * but when executed on a production box we want errors to be logged to the DB and
      * any output muted. Sever errors should generate an exception in the CodeBase project
      *
-     * @param  string  $severity
-     * @param  string  $message
-     * @param  string  $filepath
-     * @param  integer $line
+     * @param  string  $sSeverity
+     * @param  string  $sMessage
+     * @param  string  $sFilePath
+     * @param  integer $sLine
+     *
      * @return string
      */
-    public function show_php_error($severity, $message, $filepath, $line)
+    public function show_php_error($sSeverity, $sMessage, $sFilePath, $sLine)
     {
-        $_temp           = new stdClass();
-        $_temp->severity = $severity;
-        $_temp->message  = $message;
-        $_temp->filepath = $filepath;
-        $_temp->line     = $line;
+        $this->bErrorHasOccurred = true;
+        $this->aRecentErrors[]   = (object) [
+            'severity' => $sSeverity,
+            'message'  => $sMessage,
+            'filepath' => $sFilePath,
+            'line'     => $sLine,
+        ];
 
         // --------------------------------------------------------------------------
 
-        $this->error_has_occurred = true;
-        $this->recent_errors[]    = $_temp;
-
-        // --------------------------------------------------------------------------
-
-        unset($_temp);
-
-        // --------------------------------------------------------------------------
-
-        return parent::show_php_error($severity, $message, $filepath, $line);
+        return parent::show_php_error($sSeverity, $sMessage, $sFilePath, $sLine);
     }
 
     // --------------------------------------------------------------------------
@@ -100,7 +102,7 @@ class CORE_NAILS_Exceptions extends CI_Exceptions
      */
     public function error_has_occurred()
     {
-        return $this->error_has_occurred;
+        return $this->bErrorHasOccurred;
     }
 
     // --------------------------------------------------------------------------
@@ -111,7 +113,7 @@ class CORE_NAILS_Exceptions extends CI_Exceptions
      */
     public function recent_errors()
     {
-        return $this->recent_errors;
+        return $this->aRecentErrors;
     }
 
     // --------------------------------------------------------------------------
@@ -122,61 +124,85 @@ class CORE_NAILS_Exceptions extends CI_Exceptions
      */
     public function clearErrors()
     {
-        $this->error_has_occurred = false;
-        $this->recent_errors      = array();
+        $this->bErrorHasOccurred = false;
+        $this->aRecentErrors     = [];
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Renders the 404 page and halts script execution
-     * @param string $page      The URI which 404'd
-     * @param bool   $logError  Whether to log the error
+     *
+     * @param string $sPage     The URI which 404'd
+     * @param bool   $bLogError Whether to log the error
+     *
      * @return void
      */
-    public function show_404($page = '', $logError = true)
+    public function show_404($sPage = '', $bLogError = true)
     {
-        $heading = '404 Page Not Found';
-        $message = 'The page you requested was not found.';
+        $sHeading = '404 Page Not Found';
+        $sMessage = 'The page you requested was not found.';
 
-        if (empty($page)) {
-
-            $page = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+        if (empty($sPage)) {
+            $sPage = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
         }
 
         /**
          * By default we log this, but allow a dev to skip it. Additionally, skip
          * if it's a HEAD request.
          *
-         * Reasoning: I often use HEAD requests to check the existance of a file
+         * Reasoning: I often use HEAD requests to check the existence of a file
          * in JS before fetching it. I feel that these shouldn't be logged. A
-         * direct GET/POST/etc request to a non existant file is more  likely a
-         * user following a deadlink so these _should_ be logged.
+         * direct GET/POST/etc request to a non-existent file is more  likely a
+         * user following a dead link so these _should_ be logged.
          *
          * If you disagree, open up an issue and we'll work something out.
          */
 
-        $requestMethod = isset($_SERVER['REQUEST_METHOD']) ? strtoupper($_SERVER['REQUEST_METHOD']) : '';
+        $sRequestMethod = isset($_SERVER['REQUEST_METHOD']) ? strtoupper($_SERVER['REQUEST_METHOD']) : '';
 
-        if ($logError && $requestMethod != 'HEAD') {
-            log_message('error', '404 Page Not Found --> ' . $page);
+        if ($bLogError && $sRequestMethod != 'HEAD') {
+            log_message('error', '404 Page Not Found --> ' . $sPage);
         }
+
+
+        // --------------------------------------------------------------------------
+
+        /**
+         * If the route failed to resolve then there will be no instance of the CI_Controller,
+         * and as such the app will have failed to startup completely - simulate a controller
+         * being loaded if this is the case. Few of CodeIgniter's services will have been loaded
+         * so we need to load them now, we'll also boot up a controller which does nothing so
+         * that the app's constructor chain is fired.
+         */
+        if (!class_exists('CI_Controller')) {
+
+            require_once BASEPATH . 'core/Controller.php';
+
+            load_class('Output', 'core');
+            load_class('Security', 'core');
+            load_class('Input', 'core');
+            load_class('Lang', 'core');
+
+            new \Nails\Common\Controller\Nails404Controller();
+        }
+
+        // --------------------------------------------------------------------------
 
         defineConst('NAILS_IS_404', true);
 
         set_status_header(404);
 
-        $message = '<p>'.implode('</p><p>', ( ! is_array($message)) ? array($message) : $message).'</p>';
+        $sMessage = '<p>' . implode('</p><p>', (!is_array($sMessage)) ? [$sMessage] : $sMessage) . '</p>';
 
-        if (ob_get_level() > $this->ob_level + 1)
-        {
+        if (ob_get_level() > $this->ob_level + 1) {
             ob_end_flush();
         }
         ob_start();
-        include(APPPATH.'errors/error_404.php');
-        $buffer = ob_get_contents();
+        include(APPPATH . 'errors/error_404.php');
+        $sBuffer = ob_get_contents();
         ob_end_clean();
-        echo $buffer;
+        echo $sBuffer;
         exit;
     }
 }
