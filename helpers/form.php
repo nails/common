186b023e50e7587b25ef0aa774921d8451f33de6
Pablo de la Peña <p@shedcollective.org>
2018-01-11 08:59:45 +0000

    Supporting [limited] customisation of the `form_field_cdn_object_picker_multi_with_label` helper

--- helpers/form.php
+++ helpers/form.php
@@ -1030,141 +1030,136 @@ if (!function_exists('form_field_cdn_object_picker_multi_with_label')) {
      * Generates a form field containing multiple object pickers
      * @todo  when form builder is updated, ensure that other things can create custom field types
      *
-     * @param  array  $field The config array
-     * @param  string $tip   An optional tip (DEPRECATED: use $field['tip'] instead)
+     * @param  array  $aConfig The config array
+     * @param  string $sTip    An optional tip (DEPRECATED: use $field['tip'] instead)
      *
      * @return string        The form HTML
      */
-    function form_field_cdn_object_picker_multi_with_label($field, $tip = '')
+    function form_field_cdn_object_picker_multi_with_label($aConfig, $sTip = '')
     {
         //  Set var defaults
-        $_field_id         = isset($field['id']) ? $field['id'] : null;
-        $_field_type       = isset($field['type']) ? $field['type'] : 'text';
-        $_field_oddeven    = isset($field['oddeven']) ? $field['oddeven'] : null;
-        $_field_key        = isset($field['key']) ? $field['key'] : null;
-        $_field_label      = isset($field['label']) ? $field['label'] : null;
-        $_field_default    = isset($field['default']) ? $field['default'] : [];
-        $_field_bucket     = isset($field['bucket']) ? $field['bucket'] : null;
-        $_field_sub_label  = isset($field['sub_label']) ? $field['sub_label'] : null;
-        $_field_required   = isset($field['required']) ? $field['required'] : false;
-        $_field_readonly   = isset($field['readonly']) ? $field['readonly'] : false;
-        $_field_error      = isset($field['error']) ? $field['error'] : false;
-        $_field_class      = isset($field['class']) ? $field['class'] : '';
-        $_field_data       = isset($field['data']) ? $field['data'] : [];
-        $_field_info       = isset($field['info']) ? $field['info'] : false;
-        $_field_info_class = isset($field['info_class']) ? $field['info_class'] : false;
-        $_field_tip        = isset($field['tip']) ? $field['tip'] : $tip;
+        $sFieldId        = getFromArray('id', $aConfig, null);
+        $sFieldType      = getFromArray('type', $aConfig, 'text');
+        $sFieldOddEven   = getFromArray('oddeven', $aConfig, null);
+        $sFieldKey       = getFromArray('key', $aConfig, null);
+        $sFieldLabel     = getFromArray('label', $aConfig, null);
+        $sFieldDefault   = getFromArray('default', $aConfig, []);
+        $sFieldSubLabel  = getFromArray('sub_label', $aConfig, null);
+        $sFieldRequired  = getFromArray('required', $aConfig, false);
+        $sFieldReadonly  = getFromArray('readonly', $aConfig, false);
+        $sFieldError     = getFromArray('error', $aConfig, false);
+        $sFieldClass     = getFromArray('class', $aConfig, '');
+        $sFieldData      = getFromArray('data', $aConfig, []);
+        $sFieldInfo      = getFromArray('info', $aConfig, false);
+        $sFieldInfoClass = getFromArray('info_class', $aConfig, false);
+        $sFieldTip       = getFromArray('tip', $aConfig, $sTip);
+
+        $sFieldObjectKey        = getFromArray('object_key', $aConfig, 'object_id');
+        $sFieldTableLabelObject = getFromArray('table_label_object', $aConfig, 'File');
+
+        $sFieldLabelKey        = getFromArray('label_key', $aConfig, 'label');
+        $sFieldTableLabelLabel = getFromArray('table_label_label', $aConfig, 'Label');
 
         //  CDN Specific
-        $_field_bucket = isset($field['bucket']) ? $field['bucket'] : null;
+        $sFieldBucket = isset($aConfig['bucket']) ? $aConfig['bucket'] : null;
 
-        $_tip          = [];
-        $_tip['class'] = is_array($_field_tip) && isset($_field_tip['class']) ? $_field_tip['class'] : 'fa fa-question-circle fa-lg tip';
-        $_tip['rel']   = is_array($_field_tip) && isset($_field_tip['rel']) ? $_field_tip['rel'] : 'tipsy-left';
-        $_tip['title'] = is_array($_field_tip) && isset($_field_tip['title']) ? $_field_tip['title'] : null;
-        $_tip['title'] = is_string($_field_tip) ? $_field_tip : $_tip['title'];
+        $aTip = [
+            'class' => getFromArray('class', (array) $sFieldTip, 'fa fa-question-circle fa-lg tip'),
+            'rel'   => getFromArray('rel', (array) $sFieldTip, 'tipsy-left'),
+            'title' => getFromArray('title', (array) $sFieldTip, $sFieldTip),
+        ];
 
-        $_field_id_top = $_field_id ? 'id="field-' . $_field_id . '"' : '';
-        $_error        = form_error($_field_key) || $_field_error ? 'error' : '';
-        $_error_class  = $_error ? 'error' : '';
-        $_readonly     = $_field_readonly ? 'readonly="readonly"' : '';
-        $_readonly_cls = $_field_readonly ? 'readonly' : '';
+        $sFieldIdTop    = $sFieldId ? 'id="field-' . $sFieldId . '"' : '';
+        $sError         = form_error($sFieldKey) || $sFieldError ? 'error' : '';
+        $sErrorClass    = $sError ? 'error' : '';
+        $sReadonly      = $sFieldReadonly ? 'readonly="readonly"' : '';
+        $sReadonlyClass = $sFieldReadonly ? 'readonly' : '';
 
         // --------------------------------------------------------------------------
 
         //  Is the label required?
-        $_field_label .= $_field_required ? '*' : '';
+        $sFieldLabel .= $sFieldRequired ? '*' : '';
 
         //  Prep sublabel
-        $_field_sub_label = $_field_sub_label ? '<small>' . $_field_sub_label . '</small>' : '';
+        $sFieldSubLabel = $sFieldSubLabel ? '<small>' . $sFieldSubLabel . '</small>' : '';
 
         //  Has the field got a tip?
-        $_tipclass = $_tip['title'] ? 'with-tip' : '';
-        $_tip      = $_tip['title'] ? '<b class="' . $_tip['class'] . '" rel="' . $_tip['rel'] . '" title="' . htmlentities($_tip['title'], ENT_QUOTES) . '"></b>' : '';
+        $sTipClass = $aTip['title'] ? 'with-tip' : '';
+        $sTip      = $aTip['title'] ? '<b class="' . $aTip['class'] . '" rel="' . $aTip['rel'] . '" title="' . htmlentities($aTip['title'], ENT_QUOTES) . '"></b>' : '';
 
         // --------------------------------------------------------------------------
 
         //  Prep the field's attributes
-        $_attr = '';
+        $sFieldAttr = '';
 
         //  Does the field have an id?
-        $_attr .= $_field_id ? 'id="' . $_field_id . '" ' : '';
+        $sFieldAttr .= $sFieldId ? 'id="' . $sFieldId . '" ' : '';
 
         //  Any data attributes?
-        foreach ($_field_data as $attr => $value) {
-
-            $_attr .= ' data-' . $attr . '="' . $value . '"';
+        foreach ($sFieldData as $attr => $value) {
+            $sFieldAttr .= ' data-' . $attr . '="' . $value . '"';
         }
 
         // --------------------------------------------------------------------------
 
         //  Generate the field's HTML
-        $sFieldAttr = $_attr;
-        $sFieldAttr .= ' class="' . $_field_class . '" ';
-        $sFieldAttr .= $_readonly;
-
-        // Small hack to inject data-bind into the input.
-        $_field_html = cdnObjectPicker(
-            '" data-bind="attr:{name: \'download[\' + \$index() + \'][download_id]\'}, value: download_id"',
-            $_field_bucket
-        );
+        $sFieldAttr .= ' class="' . $sFieldClass . '" ';
+        $sFieldAttr .= $sReadonly;
 
         // --------------------------------------------------------------------------
 
         //  Errors
-        if ($_error && $_field_error) {
-
-            $_error = '<span class="alert alert-danger">' . $_field_error . '</span>';
-        } elseif ($_error) {
-
-            $_error = form_error($_field_key, '<span class="alert alert-danger">', '</span>');
+        if ($sError && $sFieldError) {
+            $sError = '<span class="alert alert-danger">' . $sFieldError . '</span>';
+        } elseif ($sError) {
+            $sError = form_error($sFieldKey, '<span class="alert alert-danger">', '</span>');
         }
 
         // --------------------------------------------------------------------------
 
         //  info block
-        $info_block = $_field_info ? '<small class="info ' . $_field_info_class . '">' . $_field_info . '</small>' : '';
+        $sInfoBlock = $sFieldInfo ? '<small class="info ' . $sFieldInfoClass . '">' . $sFieldInfo . '</small>' : '';
 
         // --------------------------------------------------------------------------
 
         //  Start generating the markup
-        $_field_html_id     = '<input type="hidden" name="' . $_field_key . '[{{index}}][id]" value="{{id}}">';
-        $_field_html_object = cdnObjectPicker(
-            $_field_key . '[{{index}}][object_id]',
-            $_field_bucket,
-            '{{object_id}}',
+        $sFieldHtmlId     = '<input type="hidden" name="' . $sFieldKey . '[{{index}}][id]" value="{{id}}">';
+        $sFieldHtmlObject = cdnObjectPicker(
+            $sFieldKey . '[{{index}}][' . $sFieldObjectKey . ']',
+            $sFieldBucket,
+            '{{' . $sFieldObjectKey . '}}',
             'data-index="{{index}}"'
         );
-        $_field_html_label  = '<input type="text" name="' . $_field_key . '[{{index}}][label]" value="{{label}}" data-index="{{index}}" class="js-label">';
-        $_field_html_remove = '<a href="#" class="js-cdn-multi-action-remove" data-index="{{index}}">';
-        $_field_html_remove .= '<b class="fa fa-lg fa-times-circle text-danger"></b>';
-        $_field_html_remove .= '</a>';
+        $sFieldHtmlLabel  = '<input type="text" name="' . $sFieldKey . '[{{index}}][' . $sFieldLabelKey . ']" value="{{' . $sFieldLabelKey . '}}" data-index="{{index}}" class="js-label">';
+        $sFieldHtmlRemove = '<a href="#" class="js-cdn-multi-action-remove" data-index="{{index}}">';
+        $sFieldHtmlRemove .= '<b class="fa fa-lg fa-times-circle text-danger"></b>';
+        $sFieldHtmlRemove .= '</a>';
 
         //  JS template
         $jsTpl = <<<EOT
             <tr>
                 <td>
-                    $_field_html_id
-                    $_field_html_object
+                    $sFieldHtmlId
+                    $sFieldHtmlObject
                 </td>
                 <td>
-                    $_field_html_label
+                    $sFieldHtmlLabel
                 </td>
                 <td class="text-center">
-                    $_field_html_remove
+                    $sFieldHtmlRemove
                 </td>
             </tr>
 EOT;
 
         //  Generate the initial objects
-        $_default_html = '';
-        $oMustache     = Factory::service('Mustache');
+        $sDefaultHtml = '';
+        $oMustache    = Factory::service('Mustache');
 
         if (!empty($_POST)) {
 
-            if (strpos($_field_key, '[') !== false) {
+            if (strpos($sFieldKey, '[') !== false) {
 
-                preg_match_all('/(.+?)\[([a-zA-Z0-9_\]\[]+)\]/', $_field_key, $aKeyBits);
+                preg_match_all('/(.+?)\[([a-zA-Z0-9_\]\[]+)\]/', $sFieldKey, $aKeyBits);
 
                 $aKeyBits[2] = explode('][', $aKeyBits[2][0]);
                 $aKeyBits    = array_merge($aKeyBits[1], $aKeyBits[2]);
@@ -1177,20 +1172,17 @@ EOT;
                 //  @todo find a way to not be evil
                 $aValues = eval('return !empty(' . $sPostKey . ') ? ' . $sPostKey . ' : array();');
             } else {
-
-                $aValues = isset($_POST[$_field_key]) ? $_POST[$_field_key] : [];
+                $aValues = isset($_POST[$sFieldKey]) ? $_POST[$sFieldKey] : [];
             }
-        } else {
 
-            $aValues = $_field_default;
+        } else {
+            $aValues = $sFieldDefault;
         }
 
         for ($i = 0; $i < count($aValues); $i++) {
-
-            $aValues[$i] = (array) $aValues[$i];
-
+            $aValues[$i]          = (array) $aValues[$i];
             $aValues[$i]['index'] = $i;
-            $_default_html        .= $oMustache->render($jsTpl, $aValues[$i]);
+            $sDefaultHtml         .= $oMustache->render($jsTpl, $aValues[$i]);
         }
 
         $sFieldAttr .= ' data-defaults="' . htmlentities(json_encode($aValues)) . '"';
@@ -1198,21 +1190,21 @@ EOT;
         // --------------------------------------------------------------------------
 
         $_out = <<<EOT
-            <div class="field cdn-multi cdn-multi-with-label $_error_class $_field_oddeven $_readonly_cls $_field_type" $_field_id_top $sFieldAttr>
+            <div class="field cdn-multi cdn-multi-with-label $sErrorClass $sFieldOddEven $sReadonlyClass $sFieldType" $sFieldIdTop $sFieldAttr>
                 <div>
                     <span class="label">
-                        $_field_label
-                        $_field_sub_label
+                        $sFieldLabel
+                        $sFieldSubLabel
                     </span>
-                    <span class="input $_tipclass">
+                    <span class="input $sTipClass">
                         <table class="">
                             <thead>
-                                <th width="300">File</th>
-                                <th width="*">Label</th>
+                                <th width="300">$sFieldTableLabelObject</th>
+                                <th width="*">$sFieldTableLabelLabel</th>
                                 <th width="10"></th>
                             </thead>
                             <tbody class="js-row-target">
-                                $_default_html
+                                $sDefaultHtml
                             </tbody>
                             <tbody>
                                 <tr>
@@ -1225,9 +1217,9 @@ EOT;
                             </tbody>
                         </table>
 
-                        $_tip
-                        $_error
-                        $info_block
+                        $sTip
+                        $sError
+                        $sInfoBlock
                     <span>
                 </div>
                 <script type="text/x-template" class="js-row-tpl">
