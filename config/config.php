261c4353771f81ed7c0bb2782d1f843b8009b2c6
Pablo de la Peña <p@shedcollective.org>
2018-04-03 19:02:06 +0100

    Not assuming constants are defined

--- config/config.php
+++ config/config.php
@@ -8,12 +8,7 @@ require FCPATH . 'vendor/codeigniter/framework/application/config/config.php';
  */
 
 //  Base Site URL
-if (!defined('BASE_URL')) {
-    $_ERROR = 'The <code>BASE_URL</code> constant has not been set.';
-    include NAILS_COMMON_PATH . 'errors/startup_error.php';
-}
-
-$config['base_url'] = BASE_URL;
+$config['base_url'] = defined('BASE_URL') ? BASE_URL : '/';
 
 //  Remove index.php from URL's
 $config['index_page'] = '';
@@ -31,29 +26,27 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-+@';
 $config['log_threshold'] = 1;
 
 //  Cache directory
-$config['cache_path'] = defined('CACHE_PATH') ? CACHE_PATH : APPPATH . 'cache/';
+$config['cache_path'] = defined('CACHE_PATH') ? CACHE_PATH : FCPATH . 'cache/private/';
 
 //  The encryption key
-$config['encryption_key'] = defined('APP_PRIVATE_KEY') ? md5(APP_PRIVATE_KEY): md5('');
+$config['encryption_key'] = defined('APP_PRIVATE_KEY') ? md5(APP_PRIVATE_KEY) : md5('');
 
 //  Session variables
-$config['sess_driver'] = 'database';
-$config['sess_cookie_name'] = 'nailssession';
-$config['sess_expiration'] = 1209600; // 2 weeks
-$config['sess_table_name'] = $config['sess_save_path'] = 'nails_session';
-$config['sess_match_ip'] = false;
-$config['sess_time_to_update'] = 300;
+$config['sess_driver']             = 'database';
+$config['sess_cookie_name']        = 'nailssession';
+$config['sess_expiration']         = 1209600; // 2 weeks
+$config['sess_table_name']         = $config['sess_save_path'] = 'nails_session';
+$config['sess_match_ip']           = false;
+$config['sess_time_to_update']     = 300;
 $config['sess_regenerate_destroy'] = false;
 
-
 //  Cookie related variables
-$config['cookie_httponly']  = true;
+$config['cookie_httponly'] = true;
 
 if (defined('CONF_COOKIE_DOMAIN')) {
 
     //  The developer has specified the specific domain to use for cookies
     $config['cookie_domain'] = CONF_COOKIE_DOMAIN;
-
 } else {
 
     /**
@@ -68,17 +61,16 @@ if (defined('CONF_COOKIE_DOMAIN')) {
      * if not then...
      */
 
-    $baseDomain       = getDomainFromUrl(BASE_URL);
+    $baseDomain       = getDomainFromUrl($config['base_url']);
     $secureBaseDomain = defined('SECURE_BASE_URL') ? getDomainFromUrl(SECURE_BASE_URL) : $baseDomain;
 
     if ($baseDomain == $secureBaseDomain) {
 
         //  If the two match, then define it
         $config['cookie_domain'] = $baseDomain;
-
     } else {
 
-        $_ERROR  = 'The <code>BASE_URL</code> and <code>SECURE_BASE_URL</code> ';
+        $_ERROR = 'The <code>BASE_URL</code> and <code>SECURE_BASE_URL</code> ';
         $_ERROR .= 'constants do not share the same domain, this can cause issues ';
         $_ERROR .= 'with sessions.';
 
@@ -87,5 +79,5 @@ if (defined('CONF_COOKIE_DOMAIN')) {
 }
 
 //  CSRF
-$config['csrf_token_name'] = 'nailscsrftest';   //  This is hardcoded into nails.api.js
+$config['csrf_token_name']  = 'nailscsrftest';   //  This is hardcoded into nails.api.js
 $config['csrf_cookie_name'] = 'nailscsrftoken'; //  This is hardcoded into nails.api.js
