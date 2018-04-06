929fc9d59e18e3c32d4d38d17b199d1ebcbb5ea5
Pablo de la Peña <p@shedcollective.org>
2018-04-06 09:26:57 +0100

    Added migration method + DB migration

--- src/Common/Service/Encrypt.php
+++ src/Common/Service/Encrypt.php
@@ -12,30 +12,36 @@
 
 namespace Nails\Common\Service;
 
-use \Defuse\Crypto\Crypto;
-use Nails\Common\Exception\EnvironmentException;
+use Defuse\Crypto\Crypto;
+use Defuse\Crypto\Exception\EnvironmentIsBrokenException;
+use Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException;
 use Nails\Common\Exception\Encrypt\DecodeException;
+use Nails\Common\Exception\EnvironmentException;
+use Nails\Factory;
 
 class Encrypt
 {
     /**
      * Encodes a given value using the supplied key
      *
-     * @param  mixed   $mValue The value to encode
-     * @param  boolean $sKey   The key to use for encryption
+     * @param  mixed  $mValue The value to encode
+     * @param  string $sSalt  The salt to add to the key
      *
      * @throws EnvironmentException
      * @return string
      */
-    public static function encode($mValue, $sKey = false)
+    public static function encode($mValue, $sSalt = '')
     {
         try {
             return Crypto::encryptWithPassword(
                 $mValue,
-                $sKey ?: static::getKey()
+                static::getKey($sSalt)
             );
-        } catch (\Defuse\Crypto\Exception\EnvironmentIsBrokenException $e) {
-            throw new EnvironmentException($e->getMessage(), $e->getCode());
+        } catch (EnvironmentIsBrokenException $e) {
+            throw new EnvironmentException(
+                $e->getMessage(),
+                $e->getCode()
+            );
         }
     }
 
@@ -44,35 +50,71 @@ class Encrypt
     /**
      * Decodes a given value using the supplied key
      *
-     * @param  mixed   $mValue The value to decode
-     * @param  boolean $sKey   The key to use for encryption
+     * @param  mixed  $sCipher The value to decode
+     * @param  string $sSalt   The salt to add to the key
      *
      * @throws EnvironmentException
      * @throws DecodeException
      * @return string
      */
-    public static function decode($sCipher, $sKey = false)
+    public static function decode($sCipher, $sSalt = '')
     {
         try {
             return Crypto::decryptWithPassword(
                 $sCipher,
-                $sKey ?: static::getKey()
+                static::getKey($sSalt)
             );
-        } catch (\Defuse\Crypto\Exception\EnvironmentIsBrokenException $e) {
-            throw new EnvironmentException($e->getMessage(), $e->getCode());
-        } catch (\Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException $e) {
-            throw new DecodeException($e->getMessage(), $e->getCode());
+        } catch (EnvironmentIsBrokenException $e) {
+            throw new EnvironmentException(
+                $e->getMessage(),
+                $e->getCode()
+            );
+        } catch (WrongKeyOrModifiedCiphertextException $e) {
+            throw new DecodeException(
+                $e->getMessage(),
+                $e->getCode()
+            );
         }
     }
 
     // --------------------------------------------------------------------------
 
     /**
-     * Returns the default key to use
+     * Returns the key to use, salted
+     *
+     * @param  string $sSalt The salt to add to the key
+     *
      * @return string
      */
-    public static function getKey()
+    public static function getKey($sSalt = '')
     {
-        return APP_PRIVATE_KEY;
+        return APP_PRIVATE_KEY . $sSalt;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Migrates a cipher from CI encrypt library, to Defuse\Crypto library
+     *
+     * @param string $sCipher  The cipher to migrate
+     * @param string $sOldKey  The key used to encode originally
+     * @param string $sNewSalt The salt to add to the new key
+     *
+     * @return string
+     */
+    public static function migrate($sCipher, $sOldKey, $sNewSalt = '')
+    {
+        require_once FCPATH . 'vendor/codeigniter/framework/system/libraries/Encrypt.php';
+
+        $oEncryptCi = new \CI_Encrypt();
+        $oEncrypt   = Factory::service('Encrypt');
+
+        return $oEncrypt::encode(
+            $oEncryptCi->decode(
+                $sCipher,
+                $sOldKey
+            ),
+            $sNewSalt
+        );
     }
 }
