552107f5e652f32cf74956ae42f31d39f47915e4
Pablo de la Peña <p@shedcollective.org>
2018-06-25 15:09:53 +0100

    Re-adding deleted config file

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Load the base config file from the CodeIgniter package */
+require FCPATH . 'vendor/codeigniter/framework/application/config/database.php';
+
+$db['default']['hostname'] = DEPLOY_DB_HOST;
+$db['default']['username'] = DEPLOY_DB_USERNAME;
+$db['default']['password'] = DEPLOY_DB_PASSWORD;
+$db['default']['database'] = DEPLOY_DB_DATABASE;
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['db_debug'] = true;
+$db['default']['cachedir'] = CACHE_PATH;
+$db['default']['char_set'] = 'utf8mb4';
+$db['default']['dbcollat'] = 'utf8mb4_unicode_ci';
