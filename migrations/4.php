b32d77b992052268611ef0741a128faee3a34ebc
Pablo de la Peña <p@shedcollective.org>
2018-09-14 16:42:43 +0100

    Added migration to rename `nailsapp` items in app_settings to `nails`

---
+++ migrations/4.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Migration:   4
+ * Started:     14/09/2018
+ * Finalised:   14/09/2018
+ *
+ * @package     Nails
+ * @subpackage  common
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nails\Common;
+
+use Nails\Common\Console\Migrate\Base;
+use Nails\Factory;
+
+class Migration4 extends Base
+{
+    /**
+     * Execute the migration
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query("UPDATE `{{NAILS_DB_PREFIX}}app_setting` SET `grouping` = REPLACE(`grouping`, 'nailsapp', 'nails');");
+    }
+}
