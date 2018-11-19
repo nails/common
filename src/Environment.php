caba06d77349780bff09b085eab166f698b8514b
Pablo de la Peña <p@shedcollective.org>
2018-11-19 15:27:33 +0000

    Defining environment strings in Naikls\Environment
    plus: setting environment based on test headers, environemnt variable, then config constant in that order

--- src/Environment.php
+++ src/Environment.php
@@ -16,19 +16,64 @@ use Nails\Common\Exception\EnvironmentException;
 class Environment
 {
     /**
+     * The default environments
+     */
+    const ENV_PROD  = 'PRODUCTION';
+    const ENV_STAGE = 'STAGING';
+    const ENV_DEV   = 'DEVELOPMENT';
+    const ENV_TEST  = 'TESTING';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * The current environment
+     *
+     * @var string
+     */
+    protected static $sEnvironment;
+
+    // --------------------------------------------------------------------------
+
+    /**
      * Get the current environment
+     *
      * @return string
      */
     public static function get()
     {
-        return trim(strtoupper(ENVIRONMENT));
+        if (empty(static::$sEnvironment)) {
+            $oInput = Factory::service('Input');
+            if ($oInput->header(Testing::TEST_HEADER_NAME) === Testing::TEST_HEADER_VALUE) {
+                static::set('TESTING');
+            } elseif (!empty($_ENV['ENVIRONMENT'])) {
+                static::set($_ENV['ENVIRONMENT']);
+            } else {
+                static::set(ENVIRONMENT);
+            }
+        }
+
+        return static::$sEnvironment;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Sets the active environment
+     *
+     * @param string $sEnvironment The environment to set
+     */
+    private static function set($sEnvironment)
+    {
+        static::$sEnvironment = trim(strtoupper($sEnvironment));
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Returns whether the environment is the supplied environment
+     *
      * @param  string $sEnvironment The environment to query
+     *
      * @return boolean
      */
     public static function is($sEnvironment)
@@ -40,11 +85,30 @@ class Environment
 
     /**
      * Returns whether the environment is not the supplied environment
+     *
      * @param  string $sEnvironment The environment to query
+     *
      * @return boolean
      */
     public static function not($sEnvironment)
     {
         return self::get() !== strtoupper($sEnvironment);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Lists the available environments
+     *
+     * @return array
+     */
+    public static function list()
+    {
+        return [
+            ENV_PROD,
+            ENV_STAGE,
+            ENV_DEV,
+            ENV_TEST,
+        ];
     }
 }
