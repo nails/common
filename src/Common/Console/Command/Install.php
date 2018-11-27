905fdba44de8e3953b07a3645bd88d3791b3a98c
Pablo de la Peña <p@shedcollective.org>
2018-11-27 14:12:53 +0000

    fix: Compatability with parent method

--- src/Common/Console/Command/Install.php
+++ src/Common/Console/Command/Install.php
@@ -718,7 +718,7 @@ class Install extends Base
      *
      * @return int
      */
-    protected function abort($iExitCode = self::EXIT_CODE_FAILURE, $aMessages = [])
+    protected function abort($iExitCode = self::EXIT_CODE_FAILURE, array $aMessages = [])
     {
         $aMessages[] = 'Aborting install';
         if (!empty($this->oDb) && $this->oDb->isTransactionRunning()) {
