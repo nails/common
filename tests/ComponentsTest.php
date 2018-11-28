925359372e5270aca5216155aa4cf25191892c93
Pablo de la Peña <p@shedcollective.org>
2018-11-28 23:51:43 +0000

    wip: Placeholder tests for Nails\Components

---
+++ tests/ComponentsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Nails\Common\Helper;
+
+use Nails\Components;
+use PHPUnit\Framework\TestCase;
+
+class ComponentsTest extends TestCase
+{
+    public function test_can_find_available_components()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_filter_components()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_get_component_by_slug()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_get_app_component()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_check_if_component_exists()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_list_modules()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_list_skins()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_list_drivers()
+    {
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function test_can_get_driver_instance()
+    {
+    }
+}
