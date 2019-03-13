7a29d7b5faff58129ce23a83cc89f416bad01a96
Pablo de la Peña <p@shedcollective.org>
2019-03-13 17:24:30 +0000

    chore: Redirecting when detecting the default language

--- src/Common/Event/Listener/Locale/Detect.php
+++ src/Common/Event/Listener/Locale/Detect.php
@@ -4,6 +4,8 @@ namespace Nails\Common\Event\Listener\Locale;
 
 use Nails\Common\Events;
 use Nails\Common\Events\Subscription;
+use Nails\Common\Service\HttpCodes;
+use Nails\Common\Service\Locale;
 use Nails\Factory;
 
 /**
@@ -14,6 +16,13 @@ use Nails\Factory;
 class Detect extends Subscription
 {
     /**
+     * The Locale service
+     *
+     * @var Locale
+     */
+    protected $oLocaleService;
+
+    /**
      * Detect constructor.
      */
     public function __construct()
@@ -21,16 +30,15 @@ class Detect extends Subscription
         $this
             ->setEvent(Events::SYSTEM_STARTUP)
             ->setCallback([$this, 'execute']);
+
+        $this->oLocaleService = Factory::service('Locale');
     }
 
     // --------------------------------------------------------------------------
 
     public function execute(): void
     {
-        /** @var Locale $oLocale */
-        $oLocale = Factory::service('Locale');
-
-        if ($oLocale::ENABLE_SNIFF_URL) {
+        if ($this->oLocaleService::ENABLE_SNIFF_URL) {
 
             /**
              * Manipulate the URL
@@ -40,13 +48,62 @@ class Detect extends Subscription
              * @todo (Pablo - 2019-03-08) - Check if this breaks on systems where the app isn't at the root of the domain
              */
 
-            if (array_key_exists('PATH_INFO', $_SERVER)) {
-                $_SERVER['PATH_INFO'] = '/' . preg_replace($oLocale->getUrlRegex(), '$2', ltrim($_SERVER['PATH_INFO'], '/'));
+            $oUrl = $this->parseUrl($this->getUrl());
+
+            //  If the detected language is the same as the default language then remove it form the URL and repeat
+            if ($oUrl->language === $this->oLocaleService::DEFAULT_LANGUAGE) {
+                header(
+                    'Location: /' . $oUrl->url,
+                    true,
+                    HttpCodes::STATUS_TEMPORARY_REDIRECT
+                );
+                exit(0);
             }
 
-            if (array_key_exists('REQUEST_URI', $_SERVER)) {
-                $_SERVER['REQUEST_URI'] = '/' . preg_replace($oLocale->getUrlRegex(), '$2', ltrim($_SERVER['REQUEST_URI'], '/'));
+            //  Update the $_SERVER values so the rest of the system continues as normal
+            foreach (['PATH_INFO', 'REQUEST_URL'] as $sKey) {
+                if (array_key_exists($sKey, $_SERVER)) {
+                    $_SERVER[$sKey] = '/' . $oUrl->url;
+                }
             }
+        }
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the current URL
+     *
+     * @return string
+     */
+    protected function getUrl(): string
+    {
+        return ltrim(getFromArray('PATH_INFO', $_SERVER) ?? getFromArray('REQUEST_URI', $_SERVER), '/');
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Parse the URL for supported languages
+     *
+     * @param string $sUrl The URL to parse
+     *
+     * @return \stdClass
+     */
+    protected function parseUrl(string $sUrl): \stdClass
+    {
+        preg_match($this->oLocaleService->getUrlRegex(), $sUrl, $aMatches);
+
+        if (!empty($aMatches)) {
+            return (object) [
+                'language' => !empty($aMatches[1]) ? $aMatches[1] : '',
+                'url'      => ltrim(!empty($aMatches[2]) ? $aMatches[2] : '', '/'),
+            ];
+        } else {
+            return (object) [
+                'language' => null,
+                'url'      => ltrim($sUrl, '/'),
+            ];
         }
     }
 }
