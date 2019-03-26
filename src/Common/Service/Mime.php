98f383ad5cb8cf0409522bc51ff73ce2b80b9e82
Pablo de la Peña <p@shedcollective.org>
2019-03-26 10:06:10 +0100

    Bigger source of mime types
    Using the bundled library as well as the application/Nails/CodeIgniter mime type definitions

--- src/Common/Service/Mime.php
+++ src/Common/Service/Mime.php
@@ -4,6 +4,7 @@ namespace Nails\Common\Service;
 
 use MimeType\MimeType;
 use MimeTyper\Repository\MimeDbRepository;
+use Nails\Common\Helper\ArrayHelper;
 
 /**
  * Class Mime
@@ -12,6 +13,22 @@ use MimeTyper\Repository\MimeDbRepository;
  */
 class Mime
 {
+    /**
+     * The path to the application's mime config file
+     *
+     * @var string
+     */
+    const APP_MIME_FILE = NAILS_APP_PATH . 'application/config/mimes.php';
+
+    /**
+     * The path to the Nails mime config file
+     *
+     * @var string
+     */
+    const NAILS_MIME_FILE = NAILS_COMMON_PATH . 'config/mimes.php';
+
+    // --------------------------------------------------------------------------
+
     /**
      * The mime databse
      *
@@ -25,6 +42,20 @@ class Mime
      * @var MimeType
      */
     protected $oDetector;
+
+    /**
+     * A map of extensions and their valid mime types
+     *
+     * @var string[]
+     */
+    protected static $aMapExtensionToMimes;
+
+    /**
+     * A map of mime types and their valid extensions
+     *
+     * @var string[]
+     */
+    protected static $aMapMimeToExtensions;
 
     // --------------------------------------------------------------------------
 
@@ -40,6 +71,21 @@ class Mime
     ) {
         $this->oDatabase = $oDatabase;
         $this->oDetector = $oDetector;
+
+        static::$aMapExtensionToMimes = $oDatabase->dumpExtensionToType();
+        static::$aMapMimeToExtensions = $oDatabase->dumpTypeToExtensions();
+
+        if (file_exists(static::APP_MIME_FILE)) {
+            include static::APP_MIME_FILE;
+        } else {
+            include static::NAILS_MIME_FILE;
+        }
+
+        if (!empty($mimes)) {
+            foreach ($mimes as $sExtension => $mMimes) {
+                $this->addExtension($sExtension, (array) $mMimes);
+            }
+        }
     }
 
     // --------------------------------------------------------------------------
@@ -71,6 +117,98 @@ class Mime
     // --------------------------------------------------------------------------
 
     /**
+     * Returns the mime to extension map
+     *
+     * @return array
+     */
+    public function getMimeMap(): array
+    {
+        return static::$aMapMimeToExtensions;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the extension to mime map
+     *
+     * @return array
+     */
+    public function getExtensionMap(): array
+    {
+        return static::$aMapExtensionToMimes;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Adds a new mime type to the map and its associated extensions
+     *
+     * @param string $sMime       The mime type to add
+     * @param array  $aExtensions An array of acceptable extensions
+     *
+     * @return Mime
+     */
+    public function addMime(string $sMime, array $aExtensions): self
+    {
+        $this->updateMap(static::$aMapMimeToExtensions, $sMime, $aExtensions);
+        foreach ($aExtensions as $sExtension) {
+            $this->updateMap(static::$aMapExtensionToMimes, $sExtension, [$sMime]);
+        }
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Adds a new extension to the map and its associated mime types
+     *
+     * @param string $sExtension the extension to add
+     * @param array  $aMimes     An array of acceptable mime types
+     *
+     * @return Mime
+     */
+    public function addExtension(string $sExtension, array $aMimes): self
+    {
+        $this->updateMap(static::$aMapExtensionToMimes, $sExtension, $aMimes);
+        foreach ($aMimes as $sMime) {
+            $this->updateMap(static::$aMapMimeToExtensions, $sMime, [$sExtension]);
+        }
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Updates a map array
+     *
+     * @param array  $aMap    The array to update
+     * @param string $sKey    The key to update
+     * @param array  $aValues The values to add
+     *
+     * @return Mime
+     */
+    protected function updateMap(array &$aMap, string $sKey, array $aValues): self
+    {
+        if (!array_key_exists($sKey, $aMap)) {
+            $aMap[$sKey] = [];
+        }
+
+        $aMap[$sKey] = array_values(
+            array_unique(
+                array_merge(
+                    $aMap[$sKey],
+                    $aValues
+                )
+            )
+        );
+
+        return $this;
+    }
+
+
+    // --------------------------------------------------------------------------
+
+    /**
      * Get extensions for a given mime type
      *
      * @param string $sMime The mime type to query
@@ -79,7 +217,7 @@ class Mime
      */
     public function getExtensionsForMime(string $sMime): array
     {
-        return $this->oDatabase->findExtensions($sMime);
+        return ArrayHelper::getFromArray($sMime, static::$aMapMimeToExtensions, []);
     }
 
     // --------------------------------------------------------------------------
@@ -93,6 +231,6 @@ class Mime
      */
     public function getMimesForExtension(string $sExtension): array
     {
-        return $this->oDatabase->findTypes($sExtension);
+        return ArrayHelper::getFromArray($sExtension, static::$aMapExtensionToMimes, []);
     }
 }
