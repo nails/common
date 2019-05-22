a8043bb9fde9e44349c3a457cfb692c9048f60f8
Pablo de la Peña <p@shedcollective.org>
2019-05-22 15:19:27 +0100

    Not relying on CI to instantiate the Zip class

--- src/Common/Service/Zip.php
+++ src/Common/Service/Zip.php
@@ -3,8 +3,6 @@
 /**
  * The class abstracts CI's Zip class.
  *
- * @todo - remove dependency on CI
- *
  * @package     Nails
  * @subpackage  common
  * @category    Library
@@ -14,14 +12,35 @@
 
 namespace Nails\Common\Service;
 
+use Nails\Common\Exception\NailsException;
+
 /**
  * Class Zip
+ *
  * @package Nails\Common\Service
+ *
+ * @property $zipdata           = '';
+ * @property $directory         = '';
+ * @property $entries           = 0;
+ * @property $file_num          = 0;
+ * @property $offset            = 0;
+ * @property $now               = null;
+ * @property $compression_level = 2;
+ *
+ * @method add_dir($directory)
+ * @method add_data($filepath, $data = null)
+ * @method read_file($path, $archive_filepath = false)
+ * @method read_dir($path, $preserve_filepath = true, $root_path = null)
+ * @method get_zip()
+ * @method archive($filepath)
+ * @method download($filename = 'backup.zip')
+ * @method clear_data()
  */
 class Zip
 {
     /**
      * The zip object
+     *
      * @var \CI_Zip
      */
     private $oZip;
@@ -33,9 +52,8 @@ class Zip
      */
     public function __construct()
     {
-        $oCi = get_instance();
-        $oCi->load->library('zip');
-        $this->oZip = $oCi->zip;
+        require_once NAILS_CI_SYSTEM_PATH . 'libraries/Zip.php';
+        $this->oZip = new \CI_Zip();
     }
 
     // --------------------------------------------------------------------------
@@ -43,8 +61,8 @@ class Zip
     /**
      * Route calls to the CodeIgniter Zip class
      *
-     * @param  string $sMethod The method being called
-     * @param  array $aArguments Any arguments being passed
+     * @param string $sMethod    The method being called
+     * @param array  $aArguments Any arguments being passed
      *
      * @return mixed
      */
@@ -62,7 +80,7 @@ class Zip
     /**
      * Pass any property "gets" to the CodeIgniter Zip class
      *
-     * @param  string $sProperty The property to get
+     * @param string $sProperty The property to get
      *
      * @return mixed
      */
@@ -76,13 +94,24 @@ class Zip
     /**
      * Pass any property "sets" to the CodeIgniter Zip class
      *
-     * @param  string $sProperty The property to set
-     * @param  mixed $mValue The value to set
+     * @param string $sProperty The property to set
+     * @param mixed  $mValue    The value to set
      *
      * @return void
      */
     public function __set($sProperty, $mValue)
     {
         $this->oZip->{$sProperty} = $mValue;
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function download($filename = 'backup.zip')
+    {
+        if (!function_exists('get_instance')) {
+            throw new NailsException('Cannot download; CodeIgniter not available');
+        }
+
+        parent::download($filename);
     }
 }
