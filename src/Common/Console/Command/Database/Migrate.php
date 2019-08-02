b5f1631bcb0fe543ca0c921fb05fe35992421627
Pablo de la Peña <p@shedcollective.org>
2019-08-02 15:07:43 +0100

    Further fix for DB migrations when there are _no_migrations

--- src/Common/Console/Command/Database/Migrate.php
+++ src/Common/Console/Command/Database/Migrate.php
@@ -336,12 +336,14 @@ class Migrate extends Base
         $aOut = array_values($aOut);
 
         //  Shift the app migrations onto the end so they are executed last
-        $oFirst = reset($aOut);
-        if ($oFirst->name === Components::$oAppSlug) {
-            $oApp = array_shift($aOut);
-            $aOut = array_merge($aOut, [$oApp]);
-            $aOut = array_filter($aOut);
-            $aOut = array_values($aOut);
+        if (!empty($aOut)) {
+            $oFirst = reset($aOut);
+            if ($oFirst->name === Components::$oAppSlug) {
+                $oApp = array_shift($aOut);
+                $aOut = array_merge($aOut, [$oApp]);
+                $aOut = array_filter($aOut);
+                $aOut = array_values($aOut);
+            }
         }
 
         return $aOut;
