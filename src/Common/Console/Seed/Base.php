b3d1989f4514f36230ad569a6d23839c4a580540
Pablo de la Peña <p@shedcollective.org>
2020-01-22 10:46:01 +0000

    Adds method randomItems to seeders

--- src/Common/Console/Seed/Base.php
+++ src/Common/Console/Seed/Base.php
@@ -344,6 +344,43 @@ class Base
     // --------------------------------------------------------------------------
 
     /**
+     * Returns a random selection from a supplied array
+     *
+     * @param array $aItems      The array to select from
+     * @param int   $iNum        The number of items to return
+     * @param bool  $bAllowEmpty Allow an empty result to be returned
+     *
+     * @return array
+     */
+    protected function randomItems(array $aItems, $iNum = null, $bAllowEmpty = true)
+    {
+        if ($iNum === null) {
+            $iNum = rand(
+                ($bAllowEmpty ? 0 : 1),
+                count($aItems)
+            );
+        }
+
+        if (empty($iNum)) {
+            return [];
+        }
+
+        $aKeys = (array) array_rand($aItems, $iNum);
+
+        return array_values(
+            array_filter(
+                $aItems,
+                function ($mKey) use ($aKeys) {
+                    return in_array($mKey, $aKeys);
+                },
+                ARRAY_FILTER_USE_KEY
+            )
+        );
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
      * Generates a random email address
      *
      * @return string
