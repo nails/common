0bd45bf2573b1aba544f7efa9239ae82a8614e51
Pablo de la Peña <p@shedcollective.org>
2020-01-23 16:32:18 +0000

    Updates seed command
    - Adds `—list` to list seeders
    - Adds `—fresh` to rebuild DB prior to seeding
    - Simplifies seeder filtering

--- src/Common/Console/Command/Database/Seed.php
+++ src/Common/Console/Command/Database/Seed.php
@@ -10,6 +10,7 @@ use Nails\Environment;
 use Nails\Factory;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Seed extends Base
@@ -27,12 +28,24 @@ class Seed extends Base
             ->addArgument(
                 'component',
                 InputArgument::OPTIONAL,
-                'Which component to seed from; use "list" to show all available seeders'
+                'Filter by component'
             )
             ->addArgument(
                 'class',
                 InputArgument::OPTIONAL,
-                'The seed class to execute'
+                'Filter by class name (comma separated)'
+            )
+            ->addOption(
+                'list',
+                'l',
+                InputOption::VALUE_NONE,
+                'List the seeders, rather than execute'
+            )
+            ->addOption(
+                'fresh',
+                'f',
+                InputOption::VALUE_NONE,
+                'Rebuild the database before seeding'
             );
     }
 
@@ -55,17 +68,24 @@ class Seed extends Base
 
         // --------------------------------------------------------------------------
 
-        if ($oInput->getArgument('component') == 'list') {
+        $aSeeders = $this->filterSeeders(
+            $this->getSeeders(),
+            $oInput->getArgument('component'),
+            $oInput->getArgument('class')
+        );
 
+        if (empty($aSeeders)) {
+            $oOutput->writeln('No seeders were discovered. Make one using <comment>make:db:seed</comment>');
             $oOutput->writeln('');
-            $aSeeders = $this->getSeeders();
-            if (empty($aSeeders)) {
-                $oOutput->writeln('No seeders were discovered. Make one using <comment>make:db:seed</comment>');
-                $oOutput->writeln('');
-            } else {
-                $oOutput->writeln('The following seeders are available:');
-                $this->listSeeders($aSeeders);
-            }
+            return static::EXIT_CODE_SUCCESS;
+        }
+
+        // --------------------------------------------------------------------------
+
+        if ($oInput->getOption('list')) {
+
+            $oOutput->writeln('The following seeders are available:');
+            $this->listSeeders($aSeeders);
 
             return static::EXIT_CODE_SUCCESS;
         }
@@ -87,47 +107,6 @@ class Seed extends Base
 
         // --------------------------------------------------------------------------
 
-        //  Find seeds
-        $aSeedersAll = $this->getSeeders();
-
-        //  Filter out seeders
-        $sComponent = strtolower($oInput->getArgument('component'));
-        $aClasses   = explode(',', $oInput->getArgument('class'));
-        $aClasses   = array_filter($aClasses);
-        $aClasses   = array_unique($aClasses);
-        $aSeeders   = [];
-
-        /**
-         * If component and class arguments are empty then execute all seeds
-         * If component is defined and class is empty, execute all seeds for that component
-         * If both are defined then only execute seeds which match both
-         */
-
-        if (empty($sComponent) && empty($aClasses)) {
-            $aSeeders = $aSeedersAll;
-        } elseif (!empty($sComponent) && empty($aClasses)) {
-            foreach ($aSeedersAll as $oSeeder) {
-                if ($oSeeder->component->slug === $sComponent) {
-                    $aSeeders[] = $oSeeder;
-                }
-            }
-        } else {
-            foreach ($aSeedersAll as $oSeeder) {
-                if ($oSeeder->component->slug === $sComponent && in_array($this->stripNamespace($oSeeder), $aClasses)) {
-                    $aSeeders[] = $oSeeder;
-                }
-            }
-        }
-
-        if (empty($aSeeders)) {
-            return $this->abort(
-                static::EXIT_CODE_SUCCESS,
-                [
-                    'No seeders were discovered. Make one using <comment>make:db:seed</comment>',
-                ]
-            );
-        }
-
         //  Get confirmation
         $oOutput->writeln('The following seeds will be executed in this order:');
         $this->listSeeders($aSeeders);
@@ -135,7 +114,21 @@ class Seed extends Base
         $bDoSeed = $this->confirm('Does this look OK?', true);
 
         if ($bDoSeed) {
-            $oOutput->writeln('');
+
+            if ($oInput->getOption('fresh')) {
+                $oOutput->writeln('');
+                $oOutput->write('Rebuilding database... ');
+                if ($this->callCommand('db:rebuild', [], false, true) !== static::EXIT_CODE_SUCCESS) {
+                    return $this->abort(
+                        static::EXIT_CODE_FAILURE,
+                        ['Failed to reset database.']
+                    );
+                }
+                $oOutput->writeln('<info>done!</info>');
+            } else {
+                $oOutput->writeln('');
+            }
+
             $oOutput->writeln('Executing seeds...');
             $oOutput->writeln('');
 
@@ -167,7 +160,6 @@ class Seed extends Base
         // --------------------------------------------------------------------------
 
         //  Cleaning up
-        $oOutput->writeln('');
         $oOutput->writeln('<comment>Cleaning up...</comment>');
 
         // --------------------------------------------------------------------------
@@ -212,6 +204,42 @@ class Seed extends Base
         );
 
         return $aSeedClasses;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Filters seeders by component and class
+     *
+     * @param array       $aSeeders   The seeders to filter
+     * @param string|null $sComponent The component name to filter by
+     * @param string|null $sClass     The classname to filter by
+     *
+     * @return \stdClass[]
+     */
+    protected function filterSeeders(array $aSeeders, string $sComponent = null, string $sClass = null): array
+    {
+        if ($sComponent) {
+            $aSeeders = array_filter(
+                $aSeeders,
+                function ($oSeeder) use ($sComponent) {
+                    return $sComponent === $oSeeder->component->slug;
+                }
+            );
+        }
+
+        if ($sClass) {
+
+            $aClasses = array_filter(explode(',', $sClass));
+            $aSeeders = array_filter(
+                $aSeeders,
+                function ($oSeeder) use ($aClasses) {
+                    return in_array($this->stripNamespace($oSeeder), $aClasses);
+                }
+            );
+        }
+
+        return $aSeeders;
     }
 
     // --------------------------------------------------------------------------
