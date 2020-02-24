04dadc0d64ea4c148242f3aaea96ed8704756c10
Pablo de la Peña <p@shedcollective.org>
2020-02-24 18:35:00 +0000

    Adds generic class Collection and using it as the base for Component\ClassCollection

---
+++ src/Common/Factory/Collection.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * This class contains a collection of items
+ *
+ * @package     Nails
+ * @subpackage  common
+ * @category    core
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Common\Factory;
+
+use Nails\Common\Helper\ArrayHelper;
+use Nails\Common\Helper\Tools;
+
+/**
+ * Class Collection
+ *
+ * @package Nails\Common\Factory
+ */
+abstract class Collection implements \Iterator
+{
+    /**
+     * The collection
+     *
+     * @var array
+     */
+    protected $aCollection = [];
+
+    /**
+     * The current index
+     *
+     * @var int
+     */
+    protected $iIndex = 0;
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Collection constructor.
+     *
+     * @param array $aCollection
+     */
+    public function __construct(array $aCollection = [])
+    {
+        $this->aCollection = $aCollection;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Return the current element
+     *
+     * @return mixed
+     */
+    public function current()
+    {
+        return $this->aCollection[$this->iIndex];
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Move forward to the next element
+     */
+    public function next(): void
+    {
+        $this->iIndex++;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Return the key of the current element
+     *
+     * @return bool|float|int|string|null
+     */
+    public function key()
+    {
+        return $this->iIndex;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Checks if current position is valid
+     *
+     * @return bool
+     */
+    public function valid(): bool
+    {
+        return isset($this->aCollection[$this->key()]);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Rewind the Iterator to the first element
+     */
+    public function rewind(): void
+    {
+        $this->iIndex = 0;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Reverse the collection
+     */
+    public function reverse(): void
+    {
+        $this->aCollection = array_reverse($this->aCollection);
+        $this->rewind();
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Adds a new item to the collection
+     *
+     * @param mixed $mItem The item to add to the collection
+     *
+     * @return $this
+     */
+    public function add($mItem): self
+    {
+        $this->aCollection[] = $mItem;
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Filters the collection
+     *
+     * @param callable $cFilter The callback to filter by
+     *
+     * @return Collection
+     */
+    public function filter(callable $cFilter): self
+    {
+        return new static(
+            array_filter(
+                $this->aCollection,
+                $cFilter
+            ) ?? []
+        );
+    }
+}
--- src/Common/Factory/Component/ClassCollection.php
+++ src/Common/Factory/Component/ClassCollection.php
@@ -12,6 +12,7 @@
 
 namespace Nails\Common\Factory\Component;
 
+use Nails\Common\Factory\Collection;
 use Nails\Common\Helper\ArrayHelper;
 use Nails\Common\Helper\Tools;
 
@@ -20,136 +21,22 @@ use Nails\Common\Helper\Tools;
  *
  * @package Nails\Common\Factory\Component
  */
-final class ClassCollection implements \Iterator
+final class ClassCollection extends Collection
 {
-    /**
-     * The collection
-     *
-     * @var array
-     */
-    private $aCollection = [];
-
-    /**
-     * The current index
-     *
-     * @var int
-     */
-    private $iIndex = 0;
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * ClassCollection constructor.
-     *
-     * @param array $aCollection
-     */
-    public function __construct(array $aCollection = [])
-    {
-        $this->aCollection = $aCollection;
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Return the current element
-     *
-     * @return mixed
-     */
-    public function current()
-    {
-        return $this->aCollection[$this->iIndex];
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Move forward to the next element
-     */
-    public function next(): void
-    {
-        $this->iIndex++;
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Return the key of the current element
-     *
-     * @return bool|float|int|string|null
-     */
-    public function key()
-    {
-        return $this->iIndex;
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Checks if current position is valid
-     *
-     * @return bool
-     */
-    public function valid(): bool
-    {
-        return isset($this->aCollection[$this->key()]);
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Rewind the Iterator to the first element
-     */
-    public function rewind(): void
-    {
-        $this->iIndex = 0;
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Reverse the collection
-     */
-    public function reverse(): void
-    {
-        $this->aCollection = array_reverse($this->aCollection);
-        $this->rewind();
-    }
-
-    // --------------------------------------------------------------------------
-
     /**
      * Adds a new item to the collection
      *
-     * @param string $sItem
+     * @param mixed $mItem
      *
      * @return $this
      */
-    public function add(string $sItem): self
+    public function add($mItem): self
     {
-        if (class_exists($sItem)) {
-            $this->aCollection[] = $sItem;
+        if (class_exists($mItem)) {
+            parent::add($mItem);
         }
 
         return $this;
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Filters the collection
-     *
-     * @param callable $cFilter The callback to filter by
-     *
-     * @return $this
-     */
-    public function filter(callable $cFilter): self
-    {
-        return new static(
-            array_filter(
-                $this->aCollection,
-                $cFilter
-            ) ?? []
-        );
     }
 
     // --------------------------------------------------------------------------
