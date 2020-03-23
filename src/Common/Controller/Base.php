f80591b4bf8227daecc8bf932dcd4736340696da
Pablo de la Peña <p@shedcollective.org>
2020-03-23 15:37:40 +0000

    Updates/fixes google analytics tages in base controller

--- src/Common/Controller/Base.php
+++ src/Common/Controller/Base.php
@@ -861,17 +861,20 @@ abstract class Base extends \MX_Controller
         /**
          * If a Google Analytics profile has been specified then include that too
          */
-        $sGoogleAnalyticsProfile = appSetting('google_analytics_account');
+        $sGoogleAnalyticsProfile = appSetting('google_analytics_account', 'site');
         if (!empty($sGoogleAnalyticsProfile)) {
-            $oAsset->inline(
-                "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                ga('create', '" . appSetting('google_analytics_account') . "', 'auto');
-                ga('send', 'pageview');",
-                'JS'
+            $oAsset->load(
+                'https://www.googletagmanager.com/gtag/js?id=' . $sGoogleAnalyticsProfile,
+                null,
+                'JS',
+                true
             );
+            $oAsset->inline('
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag(\'js\', new Date());
+                gtag(\'config\', \'' . $sGoogleAnalyticsProfile . '\');
+            ', 'JS');
         }
     }
 
