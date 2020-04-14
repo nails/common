f357ee7dec0b91efcf98b8f0bc63db4d9add6d1c
Pablo de la Peña <p@shedcollective.org>
2020-04-14 16:09:25 +0100

    HttpRequest: Adds support for body to POST

--- src/Common/Factory/HttpRequest/Post.php
+++ src/Common/Factory/HttpRequest/Post.php
@@ -29,6 +29,13 @@ class Post extends Get
      */
     protected $aFormParams = [];
 
+    /**
+     * The body to POST
+     *
+     * @var string
+     */
+    protected $sBody = '';
+
     // --------------------------------------------------------------------------
 
     /**
@@ -47,14 +54,40 @@ class Post extends Get
     // --------------------------------------------------------------------------
 
     /**
+     * Populates the body of the request
+     *
+     * @param string $sBody   The request body
+     * @param bool   $bIsJson Whetehr the body is JSON or not
+     */
+    public function body(string $sBody, bool $bIsJson = true): self
+    {
+        $this->sBody = $sBody;
+        if ($bIsJson) {
+            $this
+                ->setHeader('Content-Type', 'application/json')
+                ->setHeader('Content-Length', strlen($sBody));
+        }
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
      * Compile the request
      *
      * @param array $aClientConfig   The config array for the HTTP Client
      * @param array $aRequestOptions The options for the request
      */
-    protected function compile(array &$aClientConfig, array &$aRequestOptions)
+    protected function compile(array &$aClientConfig, array &$aRequestOptions): void
     {
         parent::compile($aClientConfig, $aRequestOptions);
-        $aRequestOptions['form_params'] = $this->aFormParams;
+
+        if (!empty($this->aFormParams)) {
+            $aRequestOptions['form_params'] = $this->aFormParams;
+        }
+
+        if (!empty($this->sBody)) {
+            $aRequestOptions['body'] = $this->sBody;
+        }
     }
 }
