905f6d9731a2097de536f86afb4fa5c35c203412
Pablo de la Peña <p@shedcollective.org>
2020-06-04 16:26:09 +0100

    Remoes notion of header/footer config file

--- views/structure/footer.php
+++ views/structure/footer.php
@@ -23,67 +23,21 @@ $bIs404 = (bool) Config::get('NAILS_IS_404');
 
 // --------------------------------------------------------------------------
 
-$oView   = Factory::service('View');
-$oUri    = Factory::service('Uri');
-$oConfig = Factory::service('Config');
-$oRouter = Factory::service('Router');
+/** @var View $oView */
+$oView = Factory::service('View');
 
-// --------------------------------------------------------------------------
-
-if (isset($footerOverride)) {
+if (isset($footerOverride) || isset($footer_override)) {
 
     //  Manual override
-    $oView->load($footerOverride);
+    $oView->load($footerOverride ?? $footer_override);
 
-} elseif (isset($footer_override)) {
+} elseif (file_exists(NAILS_APP_PATH . 'application/views/structure/footer/default.php')) {
 
-    //  Manual override
-    $oView->load($footer_override);
+    //  No config file, but the app has a default footer
+    $oView->load(NAILS_APP_PATH . 'application/views/structure/footer/default.php');
 
 } else {
 
-    //  Auto-detect footer if there is a config file
-    if (file_exists(NAILS_APP_PATH . 'application/config/footer_views.php')) {
-
-        $oConfig->load('footer_views');
-        $match     = false;
-        $uriString = $oUri->uri_string();
-
-        if (!$uriString) {
-            //  We're at the homepage, get the name of the default controller
-            $uriString = $oRouter->routes['default_controller'];
-        }
-
-        if ($oConfig->item('alt_footer')) {
-
-            foreach ($oConfig->item('alt_footer') as $pattern => $template) {
-
-                //  Prep the regex
-                $key = str_replace(':any', '.+', str_replace(':num', '\d+', $pattern));
-
-                //  Match found?
-                if (preg_match('#^' . preg_quote($key, '#') . '$#', $uriString)) {
-                    $match = $template;
-                    break;
-                }
-            }
-        }
-
-        //  Load the appropriate footer view
-        if ($match) {
-            $oView->load($match);
-        } else {
-            $oView->load($oConfig->item('default_footer'));
-        }
-
-    } elseif (file_exists(NAILS_APP_PATH . 'application/views/structure/footer/default.php')) {
-
-        //  No config file, but the app has a default footer
-        $oView->load('structure/footer/default');
-
-    } else {
-
-        //  No config file or app default, fall back to the default Nails. footer
-        $oView->load('structure/footer/nails-default');
-    }
+    //  No manual override or app default, fall back to the default Nails footer
+    $oView->load('structure/footer/nails-default');
 }
--- views/structure/header.php
+++ views/structure/header.php
@@ -13,6 +13,7 @@
  *
  **/
 
+use Nails\Common\Service\View;
 use Nails\Config;
 use Nails\Factory;
 
@@ -23,65 +24,21 @@ $bIs404 = (bool) Config::get('NAILS_IS_404');
 
 // --------------------------------------------------------------------------
 
-$oView   = Factory::service('View');
-$oUri    = Factory::service('Uri');
-$oConfig = Factory::service('Config');
-$oRouter = Factory::service('Router');
+/** @var View $oView */
+$oView = Factory::service('View');
 
-if (isset($headerOverride)) {
+if (isset($headerOverride) || isset($header_override)) {
 
     //  Manual override
-    $oView->load($headerOverride);
+    $oView->load($headerOverride ?? $header_override);
 
-} elseif (isset($header_override)) {
+} elseif (file_exists(NAILS_APP_PATH . 'application/views/structure/header/default.php')) {
 
-    //  Manual override
-    $oView->load($header_override);
+    //  No config file, but the app has a default header
+    $oView->load(NAILS_APP_PATH . 'application/views/structure/header/default.php');
 
 } else {
 
-    //  Auto-detect header if there is a config file
-    if (file_exists(NAILS_APP_PATH . 'application/config/header_views.php')) {
-
-        $oConfig->load('header_views');
-        $match     = false;
-        $uriString = $oUri->uri_string();
-
-        if (!$uriString) {
-            //  We're at the homepage, get the name of the default controller
-            $uriString = $oRouter->routes['default_controller'];
-        }
-
-        if ($oConfig->item('alt_header')) {
-
-            foreach ($oConfig->item('alt_header') as $pattern => $template) {
-
-                //  Prep the regex
-                $key = str_replace(':any', '.+', str_replace(':num', '\d+', $pattern));
-
-                //  Match found?
-                if (preg_match('#^' . preg_quote($key, '#') . '$#', $uriString)) {
-                    $match = $template;
-                    break;
-                }
-            }
-        }
-
-        //  Load the appropriate header view
-        if ($match) {
-            $oView->load($match);
-        } else {
-            $oView->load($oConfig->item('default_header'));
-        }
-
-    } elseif (file_exists(NAILS_APP_PATH . 'application/views/structure/header/default.php')) {
-
-        //  No config file, but the app has a default header
-        $oView->load('structure/header/default');
-
-    } else {
-
-        //  No config file or app default, fall back to the default Nails. header
-        $oView->load('structure/header/nails-default');
-    }
+    //  No manual override or app default, fall back to the default Nails header
+    $oView->load('structure/header/nails-default');
 }
