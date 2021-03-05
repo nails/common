b8d6f5d5ffd88ffeadd23edf714ac742fb42ebb0
Pablo de la Peña <p@shedcollective.org>
2021-03-05 12:02:10 +0000

    Add PHPStan

---
+++ .phpstan/constants.php
@@ -0,0 +1,5 @@
+<?php
+
+define('NAILS_COMMON_PATH', './');
+define('NAILS_CI_SYSTEM_PATH', NAILS_COMMON_PATH . 'vendor/codeigniter/framework/application/');
+define('BASEPATH', NAILS_CI_SYSTEM_PATH);
