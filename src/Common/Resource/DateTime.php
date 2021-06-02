58d1562b08bcbc78fc8b3a7408c2d79c8eb9f1e1
Pablo de la Peña <p@shedcollective.org>
2021-06-02 10:46:53 +0100

    Add methods for formatting date and time segments independently

--- src/Common/Resource/DateTime.php
+++ src/Common/Resource/DateTime.php
@@ -2,6 +2,8 @@
 
 namespace Nails\Common\Resource;
 
+use Nails\Common\Exception\FactoryException;
+
 /**
  * Class DateTime
  *
@@ -20,5 +22,41 @@ class DateTime extends Date
         if (!empty($this->raw)) {
             $this->formatted = toUserDateTime($this->raw);
         }
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the date component, formatted for the user
+     *
+     * @return string|null
+     * @throws FactoryException
+     */
+    public function formatAsUserDate(): ?string
+    {
+        /** @var \Nails\Common\Service\DateTime $oDateTimeService */
+        $oDateTimeService = \Nails\Factory::service('DateTime');
+
+        return $this->getDateTimeObject()
+            ? $this->getDateTimeObject()->format($oDateTimeService->getUserDateFormat())
+            : null;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the time component, formatted for the user
+     *
+     * @return string|null
+     * @throws FactoryException
+     */
+    public function formatAsUserTime(): ?string
+    {
+        /** @var \Nails\Common\Service\DateTime $oDateTimeService */
+        $oDateTimeService = \Nails\Factory::service('DateTime');
+
+        return $this->getDateTimeObject()
+            ? $this->getDateTimeObject()->format($oDateTimeService->getUserTimeFormat())
+            : null;
     }
 }
