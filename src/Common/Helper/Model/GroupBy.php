3d472434233be0be84ad595bb7e46313cc4c9f50
Pablo de la Peña <p@shedcollective.org>
2021-08-13 16:05:28 +0100

    Remove left over class prop

--- src/Common/Helper/Model/GroupBy.php
+++ src/Common/Helper/Model/GroupBy.php
@@ -10,7 +10,6 @@ namespace Nails\Common\Helper\Model;
 class GroupBy
 {
     private string $sColumn;
-    private string $sDirection;
 
     // --------------------------------------------------------------------------
 
