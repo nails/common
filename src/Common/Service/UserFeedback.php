390ac681d6b103e70bf23b0c4caebdfcde4b340a
Pablo de la Peña <p@shedcollective.org>
2021-09-14 17:44:42 +0100

    Correct typos

--- src/Common/Service/UserFeedback.php
+++ src/Common/Service/UserFeedback.php
@@ -70,10 +70,10 @@ class UserFeedback
 
         //  Pre-populate with flashdata
         //  @todo (Pablo - 2021-05-27) - Remove the fallback to the session
-        foreach ($this>getTypes() as $sType) {
+        foreach ($this->getTypes() as $sType) {
             $sValue = $this->oSession->getFlashData($sType);
             try {
-                if (!empty($sType)) {
+                if (!empty($sValue)) {
                     $this->set($sType, $sValue);
                 }
             } catch (\Exception $e) {
