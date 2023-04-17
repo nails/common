7c6c326159663b44d2faf322ac5fdbc3120824a8
Pablo de la Peña <p@shedcollective.org>
2023-04-17 16:39:11 +0200

    chore: Add typehints

--- helpers/email.php
+++ helpers/email.php
@@ -18,16 +18,15 @@
  */
 if (!function_exists('valid_email')) {
 
-    function valid_email($sAddress)
+    function valid_email(string $sAddress): bool
     {
         if (function_exists('filter_var')) {
-
             return (bool) filter_var($sAddress, FILTER_VALIDATE_EMAIL);
 
         } else {
 
             $sPattern = "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix";
-            return (! preg_match($sPattern, $sAddress)) ? false : true;
+            return (!preg_match($sPattern, $sAddress)) ? false : true;
         }
     }
 }
