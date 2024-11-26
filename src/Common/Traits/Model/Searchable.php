513a3d9fc7919112966e84747b249833d1e4a4aa
Pablo de la Peña <p@shedcollective.org>
2024-11-26 10:14:42 +0000

    feat: Provide opportunity to alter search term

--- src/Common/Traits/Model/Searchable.php
+++ src/Common/Traits/Model/Searchable.php
@@ -82,6 +82,13 @@ trait Searchable
 
     // --------------------------------------------------------------------------
 
+    public function getSearchTerm(array $aData): string
+    {
+        return (string) ($aData[$this->getSearchKey()] ?? '');
+    }
+
+    // --------------------------------------------------------------------------
+
     /**
      * Searches for objects, optionally paginated.
      *
@@ -123,12 +130,14 @@ trait Searchable
      */
     protected function applySearchConditionals(array &$aData): self
     {
-        $sKey = $this->getSearchKey();
-        if (empty($aData[$sKey])) {
+        $sKeywords = $this->getSearchTerm($aData);
+        if (empty($sKeywords)) {
             return $this;
         }
 
-        $sKeywords = $aData[$sKey];
+        if (empty($aData['or_like'])) {
+            $aData['or_like'] = [];
+        }
 
         if (empty($aData['or_like'])) {
             $aData['or_like'] = [];
