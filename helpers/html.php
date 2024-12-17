92462575670f209fe83b3d95be467c78646b1ab2
Pablo de la Peña <p@shedcollective.org>
2024-12-17 11:35:28 +0000

    chore: Auto-formatting

--- helpers/html.php
+++ helpers/html.php
@@ -12,19 +12,20 @@
 
 /**
  * Image
- *
  * Generates an <img /> element, modded to always include the alt attribute
  * if it's not already defined
  *
  * @access  public
- * @param   mixed
+ *
+ * @param mixed
+ *
  * @return  string
  */
 if (!function_exists('img')) {
     function img($src, $alt = false, $title = false, $index_page = true)
     {
         if (!is_array($src)) {
-            $src = array('src' => $src);
+            $src = ['src' => $src];
         }
 
         // Modded by Pablo here...
@@ -47,9 +48,9 @@ if (!function_exists('img')) {
                 $CI =& get_instance();
 
                 if ($index_page === true) {
-                    $img .= ' src="'.$CI->config->site_url($v).'" ';
+                    $img .= ' src="' . $CI->config->site_url($v) . '" ';
                 } else {
-                    $img .= ' src="'.$CI->config->slash_item('base_url').$v.'" ';
+                    $img .= ' src="' . $CI->config->slash_item('base_url') . $v . '" ';
                 }
             } else {
                 $img .= " $k=\"$v\" ";
@@ -61,26 +62,26 @@ if (!function_exists('img')) {
         //  Force SSL for local images if running on non-standard port
         if (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') {
             $site_url_ssl = str_replace('http://', 'https://', siteUrl());
-            $img = str_replace(siteUrl(), $site_url_ssl, $img);
+            $img          = str_replace(siteUrl(), $site_url_ssl, $img);
         }
 
         return $img;
     }
 }
 
-
 /**
  * Link
- *
  * Generates link to a CSS file
  *
  * @access  public
- * @param   mixed   stylesheet hrefs or an array
- * @param   string  rel
- * @param   string  type
- * @param   string  title
- * @param   string  media
- * @param   boolean should index_page be added to the css path
+ *
+ * @param mixed   stylesheet hrefs or an array
+ * @param string  rel
+ * @param string  type
+ * @param string  title
+ * @param string  media
+ * @param boolean should index_page be added to the css path
+ *
  * @return  string
  */
 if (!function_exists('link_tag')) {
@@ -95,9 +96,9 @@ if (!function_exists('link_tag')) {
             foreach ($href as $k => $v) {
                 if ($k == 'href' && strpos($v, '://') === false) {
                     if ($index_page === true) {
-                        $link .= 'href="'.$CI->config->site_url($v).'" ';
+                        $link .= 'href="' . $CI->config->site_url($v) . '" ';
                     } else {
-                        $link .= 'href="'.$CI->config->slash_item('base_url').$v.'" ';
+                        $link .= 'href="' . $CI->config->slash_item('base_url') . $v . '" ';
                     }
                 } else {
                     $link .= "$k=\"$v\" ";
@@ -107,21 +108,21 @@ if (!function_exists('link_tag')) {
             $link .= "/>";
         } else {
             if (strpos($href, '://') !== false) {
-                $link .= 'href="'.$href.'" ';
+                $link .= 'href="' . $href . '" ';
             } elseif ($index_page === true) {
-                $link .= 'href="'.$CI->config->site_url($href).'" ';
+                $link .= 'href="' . $CI->config->site_url($href) . '" ';
             } else {
-                $link .= 'href="'.$CI->config->slash_item('base_url').$href.'" ';
+                $link .= 'href="' . $CI->config->slash_item('base_url') . $href . '" ';
             }
 
-            $link .= 'rel="'.$rel.'" type="'.$type.'" ';
+            $link .= 'rel="' . $rel . '" type="' . $type . '" ';
 
-            if ($media  != '') {
-                $link .= 'media="'.$media.'" ';
+            if ($media != '') {
+                $link .= 'media="' . $media . '" ';
             }
 
-            if ($title  != '') {
-                $link .= 'title="'.$title.'" ';
+            if ($title != '') {
+                $link .= 'title="' . $title . '" ';
             }
 
             $link .= '/>';
@@ -131,17 +132,15 @@ if (!function_exists('link_tag')) {
     }
 }
 
-
-
-
 /**
  * ul_first_last
- *
  * Generates a ul element with the first and the last elements of each list given the $first_class
  * and $last_class classes respectively.
  *
  * @access  public
- * @param   mixed
+ *
+ * @param mixed
+ *
  * @return  string
  */
 if (!function_exists('list_first_last')) {
@@ -176,38 +175,38 @@ if (!function_exists('list_first_last')) {
             $attributes = $atts;
         }
 
-        $attributes = ($depth == 0) ? $attributes : null ;
+        $attributes = ($depth == 0) ? $attributes : null;
 
         // Write the opening list tag
-        $out .= "<".$type.$attributes.">\n";
+        $out .= "<" . $type . $attributes . ">\n";
 
         // Cycle through the list elements.  If an array is
         // encountered we will recursively call _list()
 
         static $_last_list_item = '';
-        $i = 0;
-        $total = count($list)-1;
+        $i     = 0;
+        $total = count($list) - 1;
         foreach ($list as $key => $val) {
             $_last_list_item = $key;
 
             $out .= str_repeat(" ", $depth + 2);
 
-            $class_f = ($i == 0)        ? $first_class  : null;
-            $class_l = ($i == $total)   ? $last_class   : null;
+            $class_f = ($i == 0) ? $first_class : null;
+            $class_l = ($i == $total) ? $last_class : null;
             //  Work out if this li is the current one absed on $current
             if (is_array($val)) {
-                $cur_val = $_last_list_item."\n";
+                $cur_val = $_last_list_item . "\n";
             } else {
                 $cur_val = $val;
             }
 
-            $cur = (preg_match('/href="\/'.$current.'/i', $cur_val)) ? ' current' : null;
+            $cur = (preg_match('/href="\/' . $current . '/i', $cur_val)) ? ' current' : null;
             $out .= "<li class=\"{$top_li} {$class_f}{$class_l}{$cur}\">";
 
             if (!is_array($val)) {
                 $out .= $val;
             } else {
-                $out .= $_last_list_item."\n";
+                $out .= $_last_list_item . "\n";
                 $out .= list_first_last($val, $type, '', $depth + 4, $first_class, $last_class);
                 $out .= str_repeat(" ", $depth + 2);
             }
@@ -221,7 +220,7 @@ if (!function_exists('list_first_last')) {
         $out .= str_repeat(" ", $depth);
 
         // Write the closing list tag
-        $out .= "</".$type.">\n";
+        $out .= "</" . $type . ">\n";
 
         return $out;
 
