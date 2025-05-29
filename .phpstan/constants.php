06be637dd34e6cbd06c163ab61934afdf7aaf9b9
Pablo de la Peña <p@shedcollective.org>
2025-05-29 15:12:49 +0100

    chore: Correct phpstan path

--- .phpstan/constants.php
+++ .phpstan/constants.php
@@ -1,5 +1,5 @@
 <?php
 
 define('NAILS_COMMON_PATH', './');
-define('NAILS_CI_SYSTEM_PATH', NAILS_COMMON_PATH . 'vendor/pocketarc/codeigniter/application/');
+define('NAILS_CI_SYSTEM_PATH', NAILS_COMMON_PATH . 'vendor/pocketarc/codeigniter/system/');
 define('BASEPATH', NAILS_CI_SYSTEM_PATH);
