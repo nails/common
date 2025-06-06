33dcbe9bfb26a2050a4ae91884b7c25126b83e9a
Pablo de la Peña <p@shedcollective.org>
2025-06-06 10:23:19 +0100

    feat: Redirect class
    Implements redirects as a class rather than a basic function. Allows for testing and a more articulate API.

---
+++ src/Common/Exception/Redirect/InvalidDestinationException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Invalid Destination Exception
+ *
+ * @package     Nails
+ * @subpackage  common
+ * @category    Exceptions
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Common\Exception\Redirect;
+
+class InvalidDestinationException extends RedirectException
+{
+}
